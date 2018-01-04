076180d3a7b0e66003dfb80abb28648744e308f6
Nathan LeSage <hendrik@zettlr.com>
2018-01-04 13:55:03 +0100

    Rewrote ZettlrWindow-class

--- source/main/zettlr-window.js
+++ source/main/zettlr-window.js
@@ -1,16 +1,20 @@
 /* Wrapper class for BrowserWindow (reduce clutter in main class) */
 
 const {dialog, BrowserWindow} = require('electron')
-const url = require('url');
-const path = require('path');
+const url                     = require('url');
+const path                    = require('path');
 
-function ZettlrWindow(parent)
+class ZettlrWindow
 {
-    this.parent = parent;
-    this.window = null;
+    constructor(parent)
+    {
+        this.parent = parent;
+        this.window = null;
+    }
 
     // Create and open a new main window
-    this.open = function() {
+    open()
+    {
         if(this.window != null) {
             // There is still a window active, so don't do anything (one-window app)
             return;
@@ -29,7 +33,7 @@ function ZettlrWindow(parent)
         });
 
         // Save this object as a parent to the browser window
-        this.window.parent = this;
+        // this.window.parent = this;
 
         // Then activate listeners.
         // and load the index.html of the app.
@@ -46,23 +50,23 @@ function ZettlrWindow(parent)
             this.window.show();
             this.window.maximize();
             this.parent.afterWindowStart();
-        })
+        });
 
         // Emitted when the window is closed.
-        this.window.on('closed', function () {
-            this.parent.close();
+        this.window.on('closed', () => {
+            this.close();
         });
 
         // Emitted when the user wants to close the window.
-        this.window.on('close', function(event) {
+        this.window.on('close', (event) => {
             // Only check, if we can close. Unless we can, abort closing process.
-            if(!this.parent.canClose()) {
+            if(!this.canClose()) {
                 event.preventDefault();
                 // Parent's (ZettlrWindow) parent (Zettlr)
-                this.parent.parent.saveAndClose();
+                this.parent.saveAndClose();
             } else {
                 // We can close - so clear down the cache in any case
-                ses = this.webContents.session;
+                let ses = this.window.webContents.session;
                 // Do not "clearCache" because that would only delete my own index files
                 ses.clearStorageData({
                     storages: [
@@ -78,74 +82,82 @@ function ZettlrWindow(parent)
 
         // Prevent closing if unable to comply
         this.window.beforeunload = (e) => {
-            if(!this.parent.canClose()) {
+            if(!this.canClose()) {
                 // Prevent closing for now.
                 e.returnValue = false;
                 // And ask the user to save changes. The parent will then re-
                 // emit the close-event which in the second round will not
                 // trigger this event.
-                this.parent.parent.saveAndClose();
+                this.parent.saveAndClose();
             }
         };
 
         // Set the application menu
         require('./main-menu.js');
-    };
+    }
     // END this.open
 
-    this.setTitle = function(newTitle) {
+    setTitle(newTitle)
+    {
         this.window.setTitle(newTitle);
-    };
+    }
 
-    this.getTitle = function() {
+    getTitle()
+    {
         return this.window.getTitle();
-    };
+    }
 
-    this.setModified = function() {
+    setModified()
+    {
         // Set the modified flag on the window if the file is edited (macOS only)
         // Function does nothing if not on macOS
         if(this.window != null) {
             this.window.setDocumentEdited(true);
         }
         // Indicate in title (for all OS)
-        title = this.window.getTitle();
+        let title = this.window.getTitle();
         if(title.substr(0, 1) != "*") {
             this.window.setTitle('*' + title);
         }
-    };
+    }
 
-    this.clearModified = function() {
+    clearModified()
+    {
         // Clear the modified flag on the window if the file is edited (macOS only)
         if(this.window != null) {
             this.window.setDocumentEdited(false);
         }
         // Indicate in title
-        title = this.window.getTitle();
+        let title = this.window.getTitle();
         if(title.substr(0, 1) == "*") {
             this.window.setTitle(title.substr(1));
         }
     }
 
-    this.getWindow = function() {
+    getWindow()
+    {
         return this.window;
-    };
+    }
 
     // FUNCTIONS CALLED FROM WITHIN EVENT LISTENERS
-    this.close = function() {
+    close()
+    {
         // Dereference the window.
         this.window = null;
-    };
+    }
 
     // The window asks if it can close itself - so lets's ask our main process
     // whether it's okay.
-    this.canClose = function() {
+    canClose()
+    {
         return this.parent.canClose();
-    };
+    }
 
     // This function belongs to the window because the dialog is attached to the
     // window. It asks the user whether or not he wants to save, omit or cancel.
-    this.askSaveChanges = function() {
-        ret = dialog.showMessageBox(this.window, {
+    askSaveChanges()
+    {
+        let ret = dialog.showMessageBox(this.window, {
             type: "question",
             title: 'Omit unsaved changes?',
             message: 'There are unsaved changes to the current file. Do you want to omit them or save?',
@@ -164,10 +176,11 @@ function ZettlrWindow(parent)
         // Well I don't want to create some constants so let's just leave it
         // with these three values.
         return ret;
-    };
+    }
 
     // Show dialog to open another folder.
-    this.askDir = function(startDir) {
+    askDir(startDir)
+    {
         return dialog.showOpenDialog(this.window, {
             title: 'Open project folder',
             defaultPath: startDir,
@@ -176,10 +189,11 @@ function ZettlrWindow(parent)
                 'createDirectory' // macOS only
             ]
         });
-    };
+    }
 
     // This function prompts the user with information.
-    this.prompt = function(options) {
+    prompt(options)
+    {
         dialog.showMessageBox(this.window, {
             type: options.type,
             buttons: [ 'Ok' ],
@@ -187,11 +201,12 @@ function ZettlrWindow(parent)
             title: options.title,
             message: options.message
         });
-    };
+    }
 
     // Ask to remove the given obj
-    this.confirmRemove = function(obj) {
-        ret = dialog.showMessageBox(this.window, {
+    confirmRemove(obj)
+    {
+        let ret = dialog.showMessageBox(this.window, {
             type: 'warning',
             buttons: [ 'Ok', 'Cancel' ],
             defaultId: 1,
@@ -200,7 +215,7 @@ function ZettlrWindow(parent)
         });
 
         return (ret == 0);
-    };
+    }
 }
 
 module.exports = ZettlrWindow;
