c29aa3e529eb1dd283be3c582870a26056472b53
Nathan LeSage <hendrik@zettlr.com>
2018-02-25 17:30:06 +0100

    Commit for changing computers

--- source/renderer/zettlr-editor.js
+++ source/renderer/zettlr-editor.js
@@ -1,16 +1,16 @@
 /**
- * @ignore
- * BEGIN HEADER
- *
- * Contains:        ZettlrEditor class
- * CVM-Role:        Model
- * Maintainer:      Hendrik Erz
- * License:         MIT
- *
- * Description:     This class controls and initialized the CodeMirror editor.
- *
- * END HEADER
- */
+* @ignore
+* BEGIN HEADER
+*
+* Contains:        ZettlrEditor class
+* CVM-Role:        Model
+* Maintainer:      Hendrik Erz
+* License:         MIT
+*
+* Description:     This class controls and initialized the CodeMirror editor.
+*
+* END HEADER
+*/
 
 // First codemirror addons
 require('codemirror/addon/mode/overlay');
@@ -35,14 +35,14 @@ require('./assets/codemirror/zettlr-plugin-footnotes.js');
 const CodeMirror = require('codemirror');
 
 /**
- * ZettlrEditor class
- */
+* ZettlrEditor class
+*/
 class ZettlrEditor
 {
     /**
-     * Instantiate the editor
-     * @param {ZettlrRenderer} parent The parent renderer element.
-     */
+    * Instantiate the editor
+    * @param {ZettlrRenderer} parent The parent renderer element.
+    */
     constructor(parent)
     {
         this.parent = parent;
@@ -52,6 +52,7 @@ class ZettlrEditor
         this.currentHash = null; // Needed for positions
         this.words = 0;
         this.fontsize = 100;
+        this.inlineImages = []; // Image widgets that are currently rendered
 
         this.cm = CodeMirror.fromTextArea(document.getElementById('cm-text'), {
             mode: {
@@ -102,6 +103,10 @@ class ZettlrEditor
 
             elt.style.textIndent = "-" + off + "px";
             elt.style.paddingLeft = (basePadding + off) + "px";
+
+            // Test for images to be rendered.
+            //console.log(line);
+            this.renderImage(line);
         });
 
         // Turn cursor into pointer while hovering link with pressed shift
@@ -137,11 +142,51 @@ class ZettlrEditor
         this.cm.refresh();
     }
 
+    renderImages()
+    {
+        let imageRE = /^!\[(.*?)\]\((.*?)\)/;
+
+        for(let widget of this.inlineImages) {
+            widget.close();
+        }
+        
+        for(let i = 0; i < this.cm.lineCount(); i++) {
+            if(!imageRE.test(line.text)) {
+                // Not an image
+                continue;
+            }
+            let match = imageRE.exec(line.text);
+            console.log(`Found image in line beginning with "${line.text.substr(0.15)}..."!`);
+            let caption = match[1] || '';
+            let url = match[2] || '';
+            if(url == '') {
+                // Nothing to render here
+                continue;
+            }
+        }
+
+        let realUrl = url;
+        // TODO: Some testing concerning the validity of the URL
+
+        console.log(`Creating image`);
+        let img = document.createElement('img');
+        img.src = realUrl;
+        img.alt = caption;
+        img.title = caption;
+        console.log(`Adding line widget!`);
+        return;
+        let lw = this.cm.addLineWidget(line, img, {noHScroll:true, above:true});
+        console.log(lw);
+        this.inlineImages.push(lw);
+
+        // TODO: Render the actual line image
+    }
+
     /**
-     * Opens a file, i.e. replaced the editor's content
-     * @param  {ZettlrFile} file The file to be renderer
-     * @return {ZettlrEditor}      Chainability.
-     */
+    * Opens a file, i.e. replaced the editor's content
+    * @param  {ZettlrFile} file The file to be renderer
+    * @return {ZettlrEditor}      Chainability.
+    */
     open(file)
     {
         if(this.currentHash != null) {
@@ -174,9 +219,9 @@ class ZettlrEditor
     }
 
     /**
-     * Closes the current file.
-     * @return {ZettlrEditor} Chainability.
-     */
+    * Closes the current file.
+    * @return {ZettlrEditor} Chainability.
+    */
     close()
     {
         this.cm.setValue('');
@@ -187,19 +232,19 @@ class ZettlrEditor
     }
 
     /**
-     * Returns the current word count in the editor.
-     * @return {Integer} The word count.
-     */
+    * Returns the current word count in the editor.
+    * @return {Integer} The word count.
+    */
     getWordCount()
     {
         return this.cm.getValue().split(' ').length;
     }
 
     /**
-     * Returns the (newly) written words since the last time this function was
-     * called.
-     * @return {Integer} The delta of the word count.
-     */
+    * Returns the (newly) written words since the last time this function was
+    * called.
+    * @return {Integer} The delta of the word count.
+    */
     getWrittenWords()
     {
         // Return the additional written words
@@ -209,12 +254,12 @@ class ZettlrEditor
     }
 
     /**
-     * Selects a word that is under the current cursor.
-     * Currently, this function is only called by the context menu class to
-     * select a word. This function only selects the word if nothing else is
-     * selected (to not fuck up some copy action someone tried to do)
-     * @return {void} Nothing to return.
-     */
+    * Selects a word that is under the current cursor.
+    * Currently, this function is only called by the context menu class to
+    * select a word. This function only selects the word if nothing else is
+    * selected (to not fuck up some copy action someone tried to do)
+    * @return {void} Nothing to return.
+    */
     selectWordUnderCursor()
     {
         // Don't overwrite selections.
@@ -228,11 +273,11 @@ class ZettlrEditor
     }
 
     /**
-     * Replaces the currently selected words. Is only called by the context
-     * menu currently.
-     * @param  {String} word The new word.
-     * @return {void}      Nothing to return.
-     */
+    * Replaces the currently selected words. Is only called by the context
+    * menu currently.
+    * @param  {String} word The new word.
+    * @return {void}      Nothing to return.
+    */
     replaceWord(word)
     {
         if(!this.cm.somethingSelected()) {
@@ -245,10 +290,10 @@ class ZettlrEditor
     }
 
     /**
-     * Displays the footnote content for a given footnote (element)
-     * @param  {jQuery} element The footnote element
-     * @return {void}         Nothing to return.
-     */
+    * Displays the footnote content for a given footnote (element)
+    * @param  {jQuery} element The footnote element
+    * @return {void}         Nothing to return.
+    */
     fntooltip(element)
     {
         // Because we highlight the formatting as well, the element's text will
@@ -270,21 +315,26 @@ class ZettlrEditor
             }
         }
 
+        if(!fnref || fnref === '') {
+            // Indicate that the footnote is empty
+            this.fntooltipbubble.html('<em>no reference text</em>');
+        } else {
+            this.fntooltipbubble.text(fnref);
+        }
+
         // Now we either got a match or an empty fnref. Anyway: display
-        this.fntooltipbubble.text(fnref);
         this.fntooltipbubble.attr('style', 'bottom:0; left:0; right:0; z-index:10000');
         this.div.append(this.fntooltipbubble);
     }
 
     /**
-     * This function builds a table of contents based on the editor contents
-     * @return {Array} An array containing objects with all headings
-     */
+    * This function builds a table of contents based on the editor contents
+    * @return {Array} An array containing objects with all headings
+    */
     buildTOC()
     {
-        let cnt = this.cm.getValue();
+        let cnt = this.cm.getValue().split('\n');
         let toc = [];
-        cnt = cnt.split('\n');
         for(let i in cnt) {
             if(/^#{1,6} /.test(cnt[i])) {
                 toc.push({
@@ -299,10 +349,10 @@ class ZettlrEditor
     }
 
     /**
-     * Small function that jumps to a specific line in the editor.
-     * @param  {Integer} line The line to pull into view
-     * @return {void}      No return.
-     */
+    * Small function that jumps to a specific line in the editor.
+    * @param  {Integer} line The line to pull into view
+    * @return {void}      No return.
+    */
     jtl(line)
     {
         // Wow. Such magic.
@@ -311,9 +361,9 @@ class ZettlrEditor
     }
 
     /**
-     * Toggles the theme.
-     * @return {ZettlrEditor} Chainability.
-     */
+    * Toggles the theme.
+    * @return {ZettlrEditor} Chainability.
+    */
     toggleTheme()
     {
         if(this.div.hasClass('dark')) {
@@ -328,9 +378,9 @@ class ZettlrEditor
     }
 
     /**
-     * Called when the directories are shown/hidden
-     * @return {ZettlrEditor} Chainability.
-     */
+    * Called when the directories are shown/hidden
+    * @return {ZettlrEditor} Chainability.
+    */
     toggleDirectories()
     {
         this.div.toggleClass('no-directories');
@@ -341,9 +391,9 @@ class ZettlrEditor
     }
 
     /**
-     * Called when the preview list is shown/hidden
-     * @return {ZettlrEditor} Chainability.
-     */
+    * Called when the preview list is shown/hidden
+    * @return {ZettlrEditor} Chainability.
+    */
     togglePreview()
     {
         this.div.toggleClass('no-preview');
@@ -354,10 +404,10 @@ class ZettlrEditor
     }
 
     /**
-     * Alter the font size of the editor.
-     * @param  {Integer} direction The direction, can be 1 (increase), -1 (decrease) or 0 (reset)
-     * @return {ZettlrEditor}           Chainability.
-     */
+    * Alter the font size of the editor.
+    * @param  {Integer} direction The direction, can be 1 (increase), -1 (decrease) or 0 (reset)
+    * @return {ZettlrEditor}           Chainability.
+    */
     zoom(direction) {
         if(direction === 0) {
             this.fontsize = 100;
@@ -370,34 +420,34 @@ class ZettlrEditor
     }
 
     /**
-     * The CodeMirror instane should open the find dialog
-     * @return {void} Nothing to return.
-     */
+    * The CodeMirror instane should open the find dialog
+    * @return {void} Nothing to return.
+    */
     openFind() { this.cm.execCommand("findPersistent"); }
 
     /**
-     * Returns the current value of the editor.
-     * @return {String} The current editor contents.
-     */
+    * Returns the current value of the editor.
+    * @return {String} The current editor contents.
+    */
     getValue() { return this.cm.getValue(); }
 
     /**
-     * Mark clean the CodeMirror instance
-     * @return {void} Nothing to return.
-     */
+    * Mark clean the CodeMirror instance
+    * @return {void} Nothing to return.
+    */
     markClean() { this.cm.markClean(); }
 
     /**
-     * Query if the editor is currently modified
-     * @return {Boolean} True, if there are no changes, false, if there are.
-     */
+    * Query if the editor is currently modified
+    * @return {Boolean} True, if there are no changes, false, if there are.
+    */
     isClean() { return this.cm.doc.isClean(); }
 
     /**
-     * Run a CodeMirror command.
-     * @param  {String} cmd The command to be passed to cm.
-     * @return {void}     Nothing to return.
-     */
+    * Run a CodeMirror command.
+    * @param  {String} cmd The command to be passed to cm.
+    * @return {void}     Nothing to return.
+    */
     runCommand(cmd) { this.cm.execCommand(cmd); }
 }
 
