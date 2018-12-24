27ac8ac6a26d9870980787f7b1e38c4dab3f232b
nathanlesage <nathan.lesage@mailbox.org>
2018-12-24 14:42:29 +0100

    Adapted ZettlrStats to StandardJS

--- source/main/zettlr-stats.js
+++ source/main/zettlr-stats.js
@@ -13,9 +13,9 @@
  * END HEADER
  */
 
-const fs            = require('fs');
-const path          = require('path');
-const {app}         = require('electron');
+const fs = require('fs')
+const path = require('path')
+const { app } = require('electron')
 
 /**
  * ZettlrStats works like the ZettlrConfig object, only with a different file.
@@ -24,115 +24,108 @@ const {app}         = require('electron');
  * (In case anyone is worried: No, there will be no transmission of stats to
  * anyone.)
  */
-class ZettlrStats
-{
-    /**
-     * Preset sane defaults and load an existing stats file if present
-     * @param {Zettlr} parent The main zettlr object.
-     */
-    constructor(parent)
-    {
-        this.parent = parent;
-        this.statsPath = app.getPath('userData');
-        this.statsFile = path.join(this.statsPath, 'stats.json');
-        this.stats = null;
+class ZettlrStats {
+  /**
+   * Preset sane defaults and load an existing stats file if present
+   * @param {Zettlr} parent The main zettlr object.
+   */
+  constructor (parent) {
+    this.parent = parent
+    this.statsPath = app.getPath('userData')
+    this.statsFile = path.join(this.statsPath, 'stats.json')
+    this.stats = null
 
-        this.statstpl = {
-            'wordCount': {}
-        }
-
-        this.load();
+    this.statstpl = {
+      'wordCount': {}
     }
 
-    /**
-     * Returns the data object.
-     * @return {Object} All statistical data.
-     */
-    getData()
-    {
-        return this.stats;
+    this.load()
+  }
+
+  /**
+   * Returns the data object.
+   * @return {Object} All statistical data.
+   */
+  getData () {
+    return this.stats
+  }
+
+  /**
+   * Load a potentially existing stats file.
+   * @return {ZettlrStats} This for chainability.
+   */
+  load () {
+    this.stats = this.statstpl
+
+    // Check if dir exists. If not, create.
+    try {
+      fs.lstatSync(this.statsPath)
+    } catch (e) {
+      fs.mkdirSync(this.statsPath)
     }
 
-    /**
-     * Load a potentially existing stats file.
-     * @return {ZettlrStats} This for chainability.
-     */
-    load()
-    {
-        this.stats = this.statstpl;
-
-        // Check if dir exists. If not, create.
-        try {
-            let s = fs.lstatSync(this.statsPath);
-        } catch(e) {
-            fs.mkdirSync(this.statsPath);
-        }
-
-        // Does the file already exist?
-        try {
-            let s = fs.lstatSync(this.statsFile);
-            this.stats = JSON.parse(fs.readFileSync(this.statsFile, { encoding: 'utf8' }));
-        } catch(e) {
-            fs.writeFileSync(this.statsFile, JSON.stringify(this.statstpl), { encoding: 'utf8' });
-            return this;
-        }
-
-        return this;
+    // Does the file already exist?
+    try {
+      fs.lstatSync(this.statsFile)
+      this.stats = JSON.parse(fs.readFileSync(this.statsFile, { encoding: 'utf8' }))
+    } catch (e) {
+      fs.writeFileSync(this.statsFile, JSON.stringify(this.statstpl), { encoding: 'utf8' })
+      return this
     }
 
-    /**
-     * Increase the word count by val
-     * @param  {Integer} val The amount of words written since the last call of this function.
-     * @return {ZettlrStats}     This for chainability.
-     */
-    updateWordCount(val)
-    {
-        if(!this.stats.hasOwnProperty('wordCount')) {
-            this.stats["wordCount"] = {};
-        }
+    return this
+  }
 
-        if(val < 0) {
-            val = 0; // Don't substract words
-        }
-
-        // For now we only need a word count
-        if(!this.stats.wordCount.hasOwnProperty(this.getDate())) {
-            this.stats.wordCount[this.getDate()] = val;
-        } else {
-            this.stats.wordCount[this.getDate()] = this.stats.wordCount[this.getDate()] + val;
-        }
+  /**
+   * Increase the word count by val
+   * @param  {Integer} val The amount of words written since the last call of this function.
+   * @return {ZettlrStats}     This for chainability.
+   */
+  updateWordCount (val) {
+    if (!this.stats.hasOwnProperty('wordCount')) {
+      this.stats['wordCount'] = {}
     }
 
-    /**
-     * Write the statistics (e.g. on app exit)
-     * @return {ZettlrStats} This for chainability.
-     */
-    save()
-    {
-        if(this.statsFile == null || this.stats == null) {
-            this.load();
-        }
-        // (Over-)write the configuration
-        fs.writeFileSync(this.statsFile, JSON.stringify(this.stats), { encoding: 'utf8' });
-
-        return this;
+    if (val < 0) {
+      val = 0 // Don't substract words
     }
 
-    /**
-     * Return today's date in the form YYYY-MM-DD
-     * @return {String} Today's date in international standard form.
-     */
-    getDate()
-    {
-        let d = new Date();
-        let yyyy = d.getFullYear();
-        let mm = d.getMonth() + 1;
-        if(mm <= 9) mm =  '0' + mm;
-        let dd = d.getDate();
-        if(dd <= 9) dd = '0' + dd;
-
-        return yyyy + '-' + mm + '-' + dd;
+    // For now we only need a word count
+    if (!this.stats.wordCount.hasOwnProperty(this.getDate())) {
+      this.stats.wordCount[this.getDate()] = val
+    } else {
+      this.stats.wordCount[this.getDate()] = this.stats.wordCount[this.getDate()] + val
     }
+  }
+
+  /**
+   * Write the statistics (e.g. on app exit)
+   * @return {ZettlrStats} This for chainability.
+   */
+  save () {
+    if (this.statsFile == null || this.stats == null) {
+      this.load()
+    }
+    // (Over-)write the configuration
+    fs.writeFileSync(this.statsFile, JSON.stringify(this.stats), { encoding: 'utf8' })
+
+    return this
+  }
+
+  /**
+   * Return today's date in the form YYYY-MM-DD
+   * @return {String} Today's date in international standard form.
+   */
+  getDate () {
+    let d = new Date()
+    let yyyy = d.getFullYear()
+    let mm = d.getMonth() + 1
+    if (mm <= 9) mm = '0' + mm
+    let dd = d.getDate()
+    if (dd <= 9) dd = '0' + dd
+
+    return yyyy + '-' + mm + '-' + dd
+  }
 }
 
-module.exports = ZettlrStats;
+module.exports = ZettlrStats
