188ff5a282c36407e12f980f2b4dc51f9234c326
nathanlesage <nathan.lesage@mailbox.org>
2018-12-24 13:51:32 +0100

    Removed the handlebars-templates from git

--- source/common/assets/tpl/other/quicklook.handlebars.js
+++
@@ -1,3 +0,0 @@
-module.exports = {"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    return "<div class=\"quicklook\">\n    <div class=\"title\">\n        <h1></h1>\n        <div class=\"find\">\n          <input type=\"text\" id=\"searchWhat\">\n        </div>\n        <div class=\"make-standalone\"></div>\n        <div class=\"close\"></div>\n    </div>\n    <div class=\"body\">\n        <textarea></textarea>\n    </div>\n</div>\n";
-},"useData":true}
--- source/common/assets/tpl/popup/textfield.handlebars.js
+++
@@ -1,9 +0,0 @@
-module.exports = {"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
-
-  return "<div>\n  <form action=\"#\" method=\"GET\">\n    <input type=\"text\" class=\"small\" value=\""
-    + alias4(((helper = (helper = helpers.val || (depth0 != null ? depth0.val : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"val","hash":{},"data":data}) : helper)))
-    + "\" placeholder=\""
-    + alias4(((helper = (helper = helpers.placeholder || (depth0 != null ? depth0.placeholder : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"placeholder","hash":{},"data":data}) : helper)))
-    + "\" name=\"name\" required>\n  </form>\n</div>\n";
-},"useData":true}
