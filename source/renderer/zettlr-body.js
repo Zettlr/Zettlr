4590731c0611b70bb69b0ae20629af120c99f746
nathanlesage <nathan.lesage@mailbox.org>
2019-02-07 22:44:52 +0100

    Fixed the writing targets

--- source/renderer/zettlr-body.js
+++ source/renderer/zettlr-body.js
@@ -227,7 +227,7 @@ class ZettlrBody {
     let targetMode = 'words'
     let targetCount = 0
 
-    if (file.hasOwnProperty('target')) {
+    if (file.hasOwnProperty('target') && file.target.mode && file.target.count) {
       // Overwrite the properties with the ones given
       targetMode = file.target.mode
       targetCount = file.target.count
