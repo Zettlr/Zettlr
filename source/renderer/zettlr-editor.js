d5b17680cc8b58da4aa5461b905f24146d495490
nathanlesage <nathan.lesage@mailbox.org>
2019-03-05 11:20:43 +0100

    Fix to the paste-with-formatting functionality

--- source/renderer/zettlr-editor.js
+++ source/renderer/zettlr-editor.js
@@ -197,11 +197,16 @@ class ZettlrEditor {
 
         // Next possibility: There's HTML formatted text in the clipboard. In
         // this case we'll be sneaky and simply exchange the plain text with
-        // the Markdown formatted version.
+        // the Markdown formatted version. We need an additional check to make
+        // sure the HTML version is indeed different than the plain text version,
+        // as some apps may write the same plain text stuff into the HTML part
+        // of the clipboard, in which case dragging it through the converter
+        // will result in unwanted behaviour.
         let html = clipboard.readHTML()
-        if (html && html.length > 0) {
+        let plain = clipboard.readText()
+        if (html && html.length > 0 && plain && html !== plain) {
           // We've got HTML, so let's fire up Showdown.js
-          let plain = this._showdown.makeMarkdown(html)
+          plain = this._showdown.makeMarkdown(html)
           // Let's update the (very likely plain) HTML text with some Markdown
           // that retains the formatting. PLEASE NOTE that we have to split the
           // resulting string as the update method expects an Array of lines,
