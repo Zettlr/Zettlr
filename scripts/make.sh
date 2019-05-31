804f4549ee574977a45f748e102fa6cdcb4856fa
nathanlesage <nathan.lesage@mailbox.org>
2019-05-31 11:08:47 +0200

    Retrofitted DMG installer background + changes to make script

--- scripts/make.sh
+++ scripts/make.sh
@@ -13,11 +13,12 @@ echo "This script will run the following commands in order:"
 echo ""
 echo "    1. Re-compile the LESS files to the final CSS"
 echo "    2. Re-compile the Handlebars templates"
-echo "    3. Compile Zettlr for Windows"
-echo "    4. Compile Zettlr for macOS"
-echo "    4. Compile Zettlr for Debian and Fedora"
-echo "    5. Generate SHA 256 checksums"
-echo "    6. Check the correctness of the checksums"
+echo "    3. Download the built-in language files"
+echo "    4. Compile Zettlr for Windows"
+echo "    5. Compile Zettlr for macOS"
+echo "    6. Compile Zettlr for Debian and Fedora"
+echo "    7. Generate SHA 256 checksums"
+echo "    8. Check the correctness of the checksums"
 echo ""
 echo ""
 
@@ -26,14 +27,22 @@ read -p "Which version is in package.json? "  pkgver
 echo "Computing with $pkgver ..."
 echo ""
 
+# Rebuild Stylesheets
 yarn less
 
+# Rebuild Templates
 yarn handlebars
 
+# fetch the most recent translations
+yarn lang:refresh
+
+# Build NSIS EXE installer
 yarn release:win
 
+# Build DMG installer
 yarn release:mac
 
+# Build .deb + .rpm
 yarn release:linux
 
 # Switch working directory to the release folder.
@@ -45,6 +54,8 @@ shasum -a 256 "Zettlr-win32-x64-$pkgver.exe" > "SHA256SUMS.txt"
 shasum -a 256 "Zettlr-macos-x64-$pkgver.dmg" >> "SHA256SUMS.txt"
 shasum -a 256 "Zettlr-linux-x64-$pkgver.deb" >> "SHA256SUMS.txt"
 shasum -a 256 "Zettlr-linux-x64-$pkgver.rpm" >> "SHA256SUMS.txt"
+
+echo ""
 
 # Test the checksums
 echo "Testing the checksums ..."
