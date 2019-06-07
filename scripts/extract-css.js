9b8ca52432e466318c4e48fb4ababfb6fe732b7b
nathanlesage <nathan.lesage@mailbox.org>
2019-06-07 15:20:20 +0200

    Small fix in the extraction script

--- scripts/extract-css.js
+++ scripts/extract-css.js
@@ -10,34 +10,34 @@ log.info(`Starting CSS extractor-script`)
 log.info(`Current working directory is ${process.cwd()}`)
 
 const CSS_PATHS = [
-  path.join(__dirname, '../source/common/assets/css/geometry.css'),
-  path.join(__dirname, '../source/common/assets/css/theme-berlin.css'),
-  path.join(__dirname, '../source/common/assets/css/theme-frankfurt.css')
+    path.join(__dirname, '../source/common/assets/css/geometry.css'),
+    path.join(__dirname, '../source/common/assets/css/theme-berlin.css'),
+    path.join(__dirname, '../source/common/assets/css/theme-frankfurt.css')
 ]
 
 // Make sure our files exist.
 for (let p of CSS_PATHS) {
-  try {
-    log.info(`Testing for existence of precompiled library ${path.basename(p)}...`)
-    fs.lstatSync(p)
-  } catch (e) {
-    log.warn(`File ${path.basename(p)} did not exist! Please run 'yarn less' first!`)
-    process.exit(0)
-  }
+    try {
+        log.info(`Testing for existence of precompiled library ${path.basename(p)}...`)
+        fs.lstatSync(p)
+    } catch (e) {
+        log.warn(`File ${path.basename(p)} did not exist! Please run 'yarn less' first!`)
+        process.exit(0)
+    }
 }
 
 // Now extract all CSS class names and IDs
 let cssDirectives = []
 for (let p of CSS_PATHS) {
-  log.info(`Concating file ${path.basename(p)} ...`)
-  try {
-    let cnt = fs.readFileSync(p, 'utf8')
-    cnt = extract(cnt)
-    log.info(`Extracted ${cnt.length} classes and IDs!`)
-    cssDirectives = cssDirectives.concat(extract(cnt))
-  } catch (e) {
-    log.error(`Could not extract IDs from file ${path.basename(p)}!`)
-  }
+    log.info(`Concating file ${path.basename(p)} ...`)
+    try {
+        let cnt = fs.readFileSync(p, 'utf8')
+        cnt = extract(cnt)
+        log.info(`Extracted ${cnt.length} classes and IDs!`)
+        cssDirectives = cssDirectives.concat(cnt)
+    } catch (e) {
+        log.error(`Could not extract IDs from file ${path.basename(p)}!`)
+    }
 }
 
 // Now make unique
@@ -49,6 +49,6 @@ log.success(`Done! Extracted ${cssDirectives.length} unique CSS classes and IDs!
 // And save to file!
 let newFile = path.join(__dirname, '../resources/css_list.md')
 fs.writeFile(newFile, '- ' + cssDirectives.join('\n- ') + '\n', 'utf8', (err) => {
-  if (err) log.error(`Could not write output file!`)
-  else log.success(`Written result to /resources/css_list.md!`)
+    if (err) log.error(`Could not write output file!`)
+    else log.success(`Written result to /resources/css_list.md!`)
 })
