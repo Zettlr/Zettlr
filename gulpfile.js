29e7b65f72a0ff27ac705e3d2532873cb21ddff6
Tobias Diez <tobiasdiez@gmx.de>
2020-04-03 12:51:35 +0200

    Incorporate PR feedback

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,12 @@
+/*
+  General structure of the gulp tasks is the following:
+    - "gulp.src(paths)" specifying the files we want to work on
+    - Some commands of the form "pipe(command)", where command is run on each file transforming it as we see fit
+    - "gulp.dest" specifying the target folder to put the output
+
+  Methods prefixed with "export" can be run using gulp, e.g. "gulp less".
+*/
+
 var gulp = require('gulp')
 var concat = require('gulp-concat')
 var sourcemaps = require('gulp-sourcemaps')
@@ -5,29 +14,30 @@ var plumber = require('gulp-plumber')
 var less = require('gulp-less')
 var rename = require('gulp-rename')
 
-gulp.task('less-geometry', function () {
+function lessGeometry () {
   return gulp.src('resources/less/geometry/geometry-main.less')
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(concat('geometry.css'))
-    .pipe(less())
+    .pipe(concat('geometry.css')) // Rename file
+    .pipe(less()) // Run less
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('source/common/assets/css/'))
-})
+}
 
-gulp.task('less-themes', function () {
+function lessThemes () {
   return gulp.src('resources/less/*/theme-main.less')
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(less())
-    .pipe(rename(function (path) {
+    .pipe(less()) // Run less
+    .pipe(rename(function (path) { // Rename files
       path.basename = path.dirname
       path.dirname = ''
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('source/common/assets/css/'))
-})
+}
 
-gulp.task('less', function () {
-  gulp.watch('resources/less/**/*', { ignoreInitial: false }, gulp.parallel('less-geometry', 'less-themes'))
-})
+exports.less = gulp.parallel(lessGeometry, lessThemes)
+exports.watch = function () {
+  gulp.watch('resources/less/**/*', { ignoreInitial: false }, exports.less)
+}
