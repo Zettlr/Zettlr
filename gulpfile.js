eed875e3ffbaafd081a4fcb41269db8ab1499b1d
Tobias Diez <tobiasdiez@gmx.de>
2020-05-11 00:47:19 +0200

    Use gulp for handlebar conversion

--- gulpfile.js
+++ gulpfile.js
@@ -13,9 +13,14 @@ var sourcemaps = require('gulp-sourcemaps')
 var plumber = require('gulp-plumber')
 var less = require('gulp-less')
 var rename = require('gulp-rename')
+var handlebars = require('gulp-handlebars')
+var wrap = require('gulp-wrap')
+
+const resourcesLessDir = 'resources/less/'
+const resourcesTemplatesDir = 'resources/templates/'
 
 function lessGeometry () {
-  return gulp.src('resources/less/geometry/geometry-main.less')
+  return gulp.src(resourcesLessDir + 'geometry/geometry-main.less')
     .pipe(plumber()) // Improve error handling
     .pipe(sourcemaps.init())
     .pipe(concat('geometry.css')) // Rename file
@@ -25,7 +30,7 @@ function lessGeometry () {
 }
 
 function lessThemes () {
-  return gulp.src('resources/less/*/theme-main.less')
+  return gulp.src(resourcesLessDir + '*/theme-main.less')
     .pipe(plumber()) // Improve error handling
     .pipe(sourcemaps.init())
     .pipe(less()) // Run less
@@ -37,7 +42,37 @@ function lessThemes () {
     .pipe(gulp.dest('source/common/assets/css/'))
 }
 
+/**
+ * Precompile handlebar templates
+ */
+function handlebarTemplates () {
+  return gulp.src(resourcesTemplatesDir + '**/*.handlebars')
+    .pipe(plumber()) // Improve error handling
+    .pipe(handlebars({
+      // Use same version of handlebars as specified in package.json
+      handlebars: require('handlebars')
+    }))
+    // The precompilation does NOT generate standalone files, we
+    // have to wrap this with module.exports to ensure it is possible to require
+    // the template at runtime!
+    .pipe(wrap('module.exports = <%= contents %>'))
+    // Change the file name from ".js" to ".handlebars.js"
+    .pipe(rename({ extname: '.handlebars.js' }))
+    .pipe(gulp.dest('source/common/assets/tpl/'))
+}
+
+/**
+ * Makes sure the runtime libraries are available to the renderer process
+ * (as the full package won't be shipped to save space).
+ */
+function handlebarRuntime () {
+  return gulp.src('node_modules/handlebars/dist/cjs/**/*')
+    .pipe(gulp.dest('source/common/assets/handlebars/'))
+}
+
 exports.less = gulp.parallel(lessGeometry, lessThemes)
+exports.handlebars = gulp.parallel(handlebarRuntime, handlebarTemplates)
 exports.watch = function () {
-  gulp.watch('resources/less/**/*', { ignoreInitial: false }, exports.less)
+  gulp.watch(resourcesLessDir + '**/*', { ignoreInitial: false }, exports.less)
+  gulp.watch(resourcesTemplatesDir + '**/*', { ignoreInitial: false }, handlebarTemplates)
 }
