7452a3ba7b427bdcc87056299a76f71d9e7b35d0
Yhancik <yhancik@users.noreply.github.com>
2020-07-21 03:26:08 +0200

    Make all paths for inserted images relative & POSIX
    This change uniforms the way images are inserted into Markdown.
    BEFORE
    - Pasting an image was inserting a relative path, dropping was inserting an absolute path
    - Pasting an image used the basename for caption, dropping was inserting the absolute path for caption
    - On Windows, all paths were using backslashes, which was making the generated Markdown files incompatible with other systems, and unfit for online uses
    
    NOW
    - The caption is always the basename (filename & extension of the picture)
    - The path is always a relative, POSIX (using forward slashes) path, which works across platforms
    
    NOTE
    Ideally, users should be able to revert that change through settings. These could be checkboxes named
    - Always use forward slashes (/) for inserted images paths
    - Always use relative paths for inserted images

--- source/renderer/zettlr-editor.js
+++ source/renderer/zettlr-editor.js
@@ -315,9 +315,15 @@ class ZettlrEditor {
 
           // Add all images.
           this._cm.setCursor(where)
+          let activeFile = this._getActiveFile()
           let str = '\n'
-          for (let p of imagesToInsert) {
-            str += `![${p.substr(p.lastIndexOf('/') + 1)}](${p})\n`
+          for (let p of imagesToInsert) {    
+            // Insert a relative path instead of an absolute one
+            let pathToInsert = path.relative(path.dirname(activeFile.fileObject.path), p)
+              
+            // Transforms Win32 paths (backslashes) into Posix paths (fwd slashes)
+            pathToInsert = path.posix.join(...pathToInsert.split(path.win32.sep))
+            str += `![${path.basename(p)}](${pathToInsert})\n`
           }
           this._cm.replaceSelection(str)
         }
