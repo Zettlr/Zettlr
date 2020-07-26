efb9fc16de2ec6a986e00109894c4649257c21d2
Tobias Diez <tobiasdiez@gmx.de>
2020-07-26 15:21:43 +0200

    Goodbye old stuff!

--- source/main.ts
+++ source/main.ts
@@ -39,11 +39,6 @@ global.log = {
 
 import { app } from 'electron';
 
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
-
 // Include the global Zettlr class
 import Zettlr from './main/zettlr'
 
