e1051a7d1e67b4dc4a81f5a13bbb5c8f6a2346de
nathanlesage <nathan.lesage@mailbox.org>
2020-10-07 15:50:39 +0200

    Move test-config to root directory

--- scripts/test-gui/make-config.js
+++ scripts/test-gui/make-config.js
@@ -1,6 +1,7 @@
 const fs = require('fs').promises
 const path = require('path')
 const YAML = require('yaml')
+const log = require('../console-colour')
 
 /**
  * Parses the test config and returns it as a JSON object
@@ -9,22 +10,16 @@ const YAML = require('yaml')
  */
 module.exports = async () => {
   const examplePath = path.join(__dirname, 'test-config.example.yml')
-  const realPath = path.join(__dirname, 'test-config.yml')
-
-  await ensureConfigFileExists()
-
-  const contents = await fs.readFile(realPath, { encoding: 'utf-8' })
-  return YAML.parse(contents)
-}
-
-async function ensureConfigFileExists () {
-  const examplePath = path.join(__dirname, 'test-config.example.yml')
-  const realPath = path.join(__dirname, 'test-config.yml')
+  const realPath = path.join(__dirname, '../../', 'test-config.yml')
 
   try {
     await fs.lstat(realPath)
   } catch (e) {
     // Real config did not exist -> copy over example
+    log.info(`Copied test-config.yml to the root directory.`)
     await fs.copyFile(examplePath, realPath)
   }
+
+  const contents = await fs.readFile(realPath, { encoding: 'utf-8' })
+  return YAML.parse(contents)
 }
