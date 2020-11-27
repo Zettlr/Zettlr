e13cc90e43ecc43558958a5020af2fbca870a61b
likeadoc <alexmannsbart@gmail.com>
2020-11-27 12:09:39 +0100

    Added mhchem rendering preview
    https://katex.org/docs/node.html

--- source/renderer/modules/markdown-editor/plugins/render-math.js
+++ source/renderer/modules/markdown-editor/plugins/render-math.js
@@ -125,9 +125,11 @@
 
         // Enable on-click closing of rendered Math elements.
         elem.onclick = (e) => { textMarker.clear() }
-
-        require('katex').render(myMarker.eq, elem, { throwOnError: false })
-
+        
+        const katex = require('katex');
+        require('katex/dist/contrib/mhchem.js'); // modify katex module to inlcude mhchem rendering
+        katex.render(myMarker.eq, elem, { throwOnError: false })
+        
         // Now the marker has obviously changed
         textMarker.changed()
       } // End for all markers
