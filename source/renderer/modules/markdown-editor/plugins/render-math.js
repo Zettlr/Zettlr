a991fed1674ee94aeca39c9c2641f36212d287b8
nathanlesage <nathan.lesage@mailbox.org>
2020-12-25 13:53:13 +0100

    Remove jQuery from math renderer

--- source/renderer/modules/markdown-editor/plugins/render-math.js
+++ source/renderer/modules/markdown-editor/plugins/render-math.js
@@ -1,4 +1,4 @@
-/* global CodeMirror $ define */
+/* global CodeMirror define */
 // This plugin renders MathJax parts in CodeMirror instances
 
 const { getBlockMathRE, getInlineMathRenderRE } = require('../../../../common/regular-expressions');
@@ -110,8 +110,8 @@ const { getBlockMathRE, getInlineMathRenderRE } = require('../../../../common/re
           continue
         }
 
-        // Use jQuery for simple creation of the DOM element
-        let elem = $('<span class="preview-math"></span>')[0]
+        let elem = document.createElement('span')
+        elem.classList.add('preview-math')
 
         let textMarker = cm.doc.markText(
           myMarker.curFrom, myMarker.curTo,
