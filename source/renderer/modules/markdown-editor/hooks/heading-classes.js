9791334d3b5c64e6bd5344f35f20a37606bb8eac
Hendrik Erz <hendrik@zettlr.com>
2021-01-18 13:42:53 +0100

    Fix #1621

--- source/renderer/modules/markdown-editor/hooks/heading-classes.js
+++ source/renderer/modules/markdown-editor/hooks/heading-classes.js
@@ -23,7 +23,6 @@ module.exports = (cm) => {
   cm.on('cursorActivity', callback)
   cm.on('viewportChange', callback)
   cm.on('optionChange', callback)
-  // cm.on('optionChange', applyHeadingClasses) // DEBUG
 }
 
 function applyHeadingClasses (cm) {
@@ -32,23 +31,29 @@ function applyHeadingClasses (cm) {
   for (let i = viewport.from; i < viewport.to; i++) {
     const line = cm.getLine(i)
 
-    // Then remove all header styles
-    for (let x = 1; x < 7; x++) {
-      cm.removeLineClass(i, 'wrap', `size-header-${x}`)
-    }
+    const headerClass = retrieveHeaderClass(cm, i)
 
     // Only re-apply a header class if allowed.
     if (cm.getModeAt({ 'line': i, 'ch': 0 }).name !== 'markdown') {
+      if (headerClass > 0) {
+        removeHeaderClass(cm, i, headerClass)
+      }
       continue
     }
 
     // Then re-add the header classes as appropriate.
     let match = /^(#{1,6}) /.exec(line)
     if (match) {
-      cm.addLineClass(i, 'wrap', `size-header-${match[1].length}`)
+      maybeUpdateHeaderClass(cm, i, match[1].length)
+      continue // Finished
+    } else if (headerClass > 0) {
+      removeHeaderClass(cm, i, headerClass)
+      continue
     }
 
-    if (i === 0) continue // No need to check for Setext header
+    if (i === 0) {
+      continue // No need to check for Setext header
+    }
 
     // Check for Setext headers. According to the CommonMark
     // spec: At most 3 preceeding spaces, no internal spaces
@@ -84,8 +89,56 @@ function applyHeadingClasses (cm) {
 
       // Add the correct line classes to both lines
       for (let line = begin; line <= i; line++) {
-        cm.addLineClass(line, 'wrap', `size-header-${level}`)
+        maybeUpdateHeaderClass(cm, line, level)
       }
     }
+  }
+}
+
+/**
+ * Retrieves a heading class if there is one. Returns 0 if no header class was
+ * detected.
+ *
+ * @param   {CodeMirror}  cm    The CodeMirror instance
+ * @param   {number}      line  The line number
+ *
+ * @return  {number}            The header class, between 1 and 6, or 0 if no class was found.
+ */
+function retrieveHeaderClass (cm, line) {
+  const lineInfo = cm.doc.lineInfo(line)
+  const match = /size-header-([1-6])/.exec(lineInfo.wrapClass)
+
+  if (match !== null) {
+    return parseInt(match[1], 10)
+  } else {
+    return 0
+  }
+}
+
+/**
+ * Removes the given header class from the line
+ *
+ * @param   {CodeMirror}  cm           The CodeMirror instance
+ * @param   {number}      line         The affected line
+ * @param   {number}      classNumber  The class to remove
+ */
+function removeHeaderClass (cm, line, classNumber) {
+  cm.doc.removeLineClass(line, 'wrap', `size-header-${classNumber}`)
+}
+
+/**
+ * Updates the line class to newClass if applicable
+ *
+ * @param   {CodeMirror}  cm        The CodeMirror instance
+ * @param   {number}      line      The line
+ * @param   {number}      newClass  The new class 1-6 to be applied
+ */
+function maybeUpdateHeaderClass (cm, line, newClass) {
+  const headerClass = retrieveHeaderClass(cm, line)
+  if (headerClass !== newClass) {
+    if (headerClass > 0) {
+      removeHeaderClass(cm, line, headerClass)
+    }
+    cm.doc.addLineClass(line, 'wrap', `size-header-${newClass}`)
   }
 }
