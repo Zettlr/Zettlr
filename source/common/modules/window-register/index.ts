c92435254b31c9d2afa4c11fb646d85443c325a3
Hendrik Erz <hendrik@zettlr.com>
2021-02-06 17:56:13 +0100

    Remove dead code from window registration

--- source/common/modules/window-register/index.ts
+++ source/common/modules/window-register/index.ts
@@ -30,16 +30,6 @@ export default function windowRegister (options?: RegistrationOptions): void {
   // Load the clarity icons
   loadIcons().catch(e => { console.error(e) })
 
-  // Prevent any funny navigation -- Zettlr does not allow any other file
-  // to be open here, so if anyone wants to navigate, open the link externally
-  // instead.
-  // TODO: Currently, this also prevents reloads
-  // window.addEventListener('beforeunload', (event) => {
-  //   event.preventDefault()
-  //   event.returnValue = false // equivalent to `return false` but not recommended
-  //   return false
-  // })
-
   // Determine if the menubar should be shown (default: yes)
   let shouldShowMenubar: boolean = true
   if (options !== undefined) {
