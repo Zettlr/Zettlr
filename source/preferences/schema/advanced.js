d3fff11c747e7a42836cbe3575a2c2d229ccd902
Hendrik Erz <hendrik@zettlr.com>
2021-02-16 19:40:45 +0100

    Migrate schemas to their own files

---
+++ source/preferences/schema/advanced.js
@@ -0,0 +1,63 @@
+import { trans } from '../../common/i18n'
+
+export default {
+  fieldsets: [
+    [
+      {
+        type: 'text',
+        label: trans('dialog.preferences.filename_generator'),
+        model: 'newFileNamePattern',
+        info: 'Variables: %id, %Y, %y, %M, %D, %W, %h, %m, %s, %X, %uuid4'
+      },
+      {
+        type: 'checkbox',
+        label: trans('dialog.preferences.new_file_dont_prompt'),
+        model: 'newFileDontPrompt'
+      }
+    ],
+    [
+      {
+        type: 'checkbox',
+        label: trans('dialog.preferences.debug'),
+        model: 'debug'
+      },
+      {
+        type: 'checkbox',
+        label: trans('dialog.preferences.checkForBeta'),
+        model: 'checkForBeta'
+      },
+      {
+        type: 'checkbox',
+        label: trans('dialog.preferences.use_native_appearance'),
+        model: 'window.nativeAppearance'
+      },
+      {
+        type: 'checkbox',
+        label: trans('dialog.preferences.delete_on_fail'),
+        model: 'system.deleteOnFail'
+      }
+    ],
+    [
+      {
+        type: 'checkbox',
+        label: trans('dialog.preferences.watchdog_checkbox_label'),
+        model: 'watchdog.activatePolling'
+      },
+      {
+        type: 'number',
+        label: trans('dialog.preferences.watchdog_threshold_label'),
+        model: 'watchdog.stabilityThreshold'
+      }
+    ]
+  ],
+  fields: [
+    // {
+    //   type: 'list',
+    //   label: 'Other files',
+    //   listOptions: {},
+    //   options: {
+    //     '.pdf': 'PDF' // TODO
+    //   }
+    // },
+  ]
+}
