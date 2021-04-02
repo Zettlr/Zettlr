7a37826d222032c9bc287efb6fc7221fd5a76f59
Hendrik Erz <hendrik@zettlr.com>
2021-04-02 11:18:21 +0200

    Fix incorrect failing of the Pandoc script

--- scripts/get-pandoc.sh
+++ scripts/get-pandoc.sh
@@ -61,7 +61,7 @@ if [ "$PLATFORM" == "darwin" ] && [ "$ARCH" == "arm" ]
 then
     # There's no ARM Pandoc. However, x64 Pandoc binaries work well on macOS ARM
     # due to the existence of Rosetta. So we can safely take that one.
-    echo "Currently there is no ARM-release for macOS. Falling back to x64 ..." 1>&2 # Redirect echo's stdout to stderr
+    echo "Currently there is no ARM-release for macOS. Falling back to x64 ..."
     ARCH="x64" # In case we're at some point referring to ARCH again.
     RELEASE="$PANDOC_MACOS_X64"
 elif [ "$PLATFORM" == "darwin" ] && [ "$ARCH" == "x64" ]
@@ -81,12 +81,6 @@ elif [ "$PLATFORM" == "win32" ] && [ "$ARCH" == "x64" ]
 then
   RELEASE="$PANDOC_WIN32_X64"
 fi
-
-# if [ "$RELEASE" == "" ]
-# then
-#   echo "No release given! Please provide a release: linux, win32, darwin"
-#   exit
-# fi
 
 # First, cd into the resources directory
 cd "$(dirname "$0")"
