098e29829deffa630633124f071331439dba7785
a1808951 <69383460+a1808951@users.noreply.github.com>
2021-05-06 23:08:31 +0930

    fixed  increasing the webpack size issue

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -19,7 +19,7 @@ module.exports = {
         { from: 'source/main/modules/export/assets/template.revealjs.htm', to: 'assets' },
         { from: 'source/main/modules/export/assets/revealjs-styles', to: 'assets/revealjs-styles' },
         { from: 'source/main/modules/window-manager/assets/icons', to: 'assets/icons' },
-        { from: 'resources/icons', to: 'assets/icons' }
+        { from: 'resources/icons/png', to: 'assets/icons/png' }
       ]
     })
   ],
