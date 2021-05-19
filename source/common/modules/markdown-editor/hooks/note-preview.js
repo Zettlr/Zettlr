fb6cc5df1caef921db5e33599c9845d894c7b21d
Derkness <tlpapae@gmail.com>
2021-05-20 01:22:12 +0930

    Added more lines to display. Added html tag filtering.

--- source/common/modules/markdown-editor/hooks/note-preview.js
+++ source/common/modules/markdown-editor/hooks/note-preview.js
@@ -52,16 +52,30 @@ module.exports = (cm) => {
           ipcRenderer.invoke('application', { command: 'get-file-contents', payload: filepath })
             .then((descriptorWithContent) => {
 
+              // Remove html from content so html is displayed normally. escape all special chars
+              descriptorWithContent.content = descriptorWithContent.content.replace(/(<([^>]+)>)/gi, "")
+
               // Get the contents of the file such as:
-              content = descriptorWithContent.content.substring(0, 50) // The text, up to 50 chars
+              content = descriptorWithContent.content.substring(0, 50) // 4 lines of 50
+              if (descriptorWithContent.content.length >= 50) {
+                content += '\n' + descriptorWithContent.content.substring(50, 100)
+                if (descriptorWithContent.content.length >= 50) {
+                  content += '\n' + descriptorWithContent.content.substring(100, 150)
+                  if (descriptorWithContent.content.length >= 50) {
+                    content += '\n' + descriptorWithContent.content.substring(150, 200)
+                  }
+                }
+              }
               wordCount = descriptorWithContent.wordCount // The word count
               title = descriptorWithContent.name // The file name
 
               dateDif = Date.now() - descriptorWithContent.modtime
               days = Math.floor(dateDif / (86400000)) // The days since modification
 
+              // Remove html from content so code is displayed normally. escape all special chars
+
               // On ready, show a tooltip with the note contents
-              tooltip.setContent(`File Name: ${title}<br>${content}<br>Word Count: ${wordCount}<br> ${days} Days Since Modification`)
+              tooltip.setContent(`File Name: \"${title}\"<br>\"${content}\"<br>Word Count: ${wordCount}<br> ${days} Days Since Modification`)
             }).catch(err => console.error("File content get error: " + err));
         } else {
           tooltip.setContent(`File Not Found`)
