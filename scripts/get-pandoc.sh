d3ab5d36069237fa22618af65d9aa73843de1067
Hendrik Erz <hendrik@zettlr.com>
2021-08-22 14:24:59 +0200

    chore(pandoc): Bump Pandoc to 2.14.2

--- scripts/get-pandoc.sh
+++ scripts/get-pandoc.sh
@@ -5,10 +5,10 @@
 # because this gives us more control over the built-in pandoc binary.
 
 # Retrieve the versions from https://github.com/jgm/pandoc/releases/latest
-PANDOC_LINUX_X64="https://github.com/jgm/pandoc/releases/download/2.14.1/pandoc-2.14.1-linux-amd64.tar.gz"
-PANDOC_LINUX_ARM="https://github.com/jgm/pandoc/releases/download/2.14.1/pandoc-2.14.1-linux-arm64.tar.gz"
-PANDOC_WIN32_X64="https://github.com/jgm/pandoc/releases/download/2.14.1/pandoc-2.14.1-windows-x86_64.zip"
-PANDOC_MACOS_X64="https://github.com/jgm/pandoc/releases/download/2.14.1/pandoc-2.14.1-macOS.zip"
+PANDOC_LINUX_X64="https://github.com/jgm/pandoc/releases/download/2.14.2/pandoc-2.14.2-linux-amd64.tar.gz"
+PANDOC_LINUX_ARM="https://github.com/jgm/pandoc/releases/download/2.14.2/pandoc-2.14.2-linux-arm64.tar.gz"
+PANDOC_WIN32_X64="https://github.com/jgm/pandoc/releases/download/2.14.2/pandoc-2.14.2-windows-x86_64.zip"
+PANDOC_MACOS_X64="https://github.com/jgm/pandoc/releases/download/2.14.2/pandoc-2.14.2-macOS.zip"
 
 # Prepare our global variables
 PLATFORM="$1"
