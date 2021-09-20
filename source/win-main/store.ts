b1db8b817f4ab3456297f26ecef74bd605d58088
Victor Sanchez Urrutia <v.sanchez.urrutia@gmail.com>
2021-09-20 08:38:14 -0500

    Made changes to satisfy eslint. Some changes may be done more elegantly perhaps.

--- source/win-main/store.ts
+++ source/win-main/store.ts
@@ -25,7 +25,6 @@ import { MDFileMeta, CodeFileMeta, DirMeta } from '../main/modules/fsal/types'
 const path = (window as any).path
 const ipcRenderer = (window as any).ipc as Electron.IpcRenderer
 
-
 interface FSALEvent {
   event: 'remove'|'add'|'change'
   path: string
@@ -58,7 +57,10 @@ function findPathDescriptor (targetPath: string, tree: any, treatAsAttachment: b
       if (targetPath === descriptor.path) {
         // We have the correct element
         return descriptor
-      } else if (targetPath.startsWith(descriptor.path+path.sep) && descriptor.type === 'directory') {
+        // I "fixed" eslint's complaint that both descriptor.path and path.sep must be explicit strings, but since I am new to JavaScript and 
+        // its relationship to node.js, why do I need to do this? Isn't path.sep defined in node.js as to return a string? Perhaps, when building the 
+        // directory "tree" should paths be coerced to strings there? 
+      } else if (targetPath.startsWith((descriptor.path as string) + (path.sep as string)) && descriptor.type === 'directory') {
         // We have the correct tree
         return findPathDescriptor(targetPath, descriptor[prop], treatAsAttachment)
       }
@@ -74,7 +76,7 @@ function findPathDescriptor (targetPath: string, tree: any, treatAsAttachment: b
       if (targetPath === child.path) {
         // We got the correct child
         return child
-      } else if (targetPath.startsWith(child.path+path.sep) && child.type === 'directory') {
+      } else if (targetPath.startsWith((child.path as string) + (path.sep as string)) && child.type === 'directory') {
         // Traverse further down
         return findPathDescriptor(targetPath, child[prop], treatAsAttachment)
       }
