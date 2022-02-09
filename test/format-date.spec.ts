136447909543e0dc79269555255c36383b207329
Hendrik Erz <hendrik@zettlr.com>
2022-02-09 16:28:08 +0100

    fix: Date formatter unit test failing
    The new unit tests for the date formatter were failing
    because the CI server is in a different timezone than
    my MacBook

--- test/format-date.spec.ts
+++ test/format-date.spec.ts
@@ -18,22 +18,22 @@ import formatDate from '../source/common/util/format-date'
 const tests = [
   // First general tests
   {
-    input: Date.parse('09 Feb 2022 10:59:23 GMT'),
+    input: Date.parse('09 Feb 2022 10:59:23'),
     relative: false,
     locale: 'en-US',
-    expected: 'February 9, 2022 at 11:59 AM'
+    expected: 'February 9, 2022 at 10:59 AM'
   },
   {
-    input: Date.parse('09 Feb 2022 10:59:23 GMT'),
+    input: Date.parse('09 Feb 2022 10:59:23'),
     relative: false,
     locale: 'de-DE',
-    expected: '9. Februar 2022 um 11:59'
+    expected: '9. Februar 2022 um 10:59'
   },
   {
-    input: Date.parse('01 Jan 1970 00:00:00 GMT'),
+    input: Date.parse('01 Jan 1970 00:00:00'),
     relative: false,
     locale: 'en-US',
-    expected: 'January 1, 1970 at 1:00 AM'
+    expected: 'January 1, 1970 at 12:00 AM'
   },
   // Now, some relative tests.
   // NOTE: We cannot currently test for "just now" (< 1 min) since that depends
