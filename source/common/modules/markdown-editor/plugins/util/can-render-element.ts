e4764f07f4cb9d54553345e545271c45ad5df771
Hendrik Erz <hendrik@zettlr.com>
2022-02-23 20:49:35 +0100

    chore: Add function documentation

--- source/common/modules/markdown-editor/plugins/util/can-render-element.ts
+++ source/common/modules/markdown-editor/plugins/util/can-render-element.ts
@@ -1,5 +1,30 @@
+/**
+ * @ignore
+ * BEGIN HEADER
+ *
+ * Contains:        canRenderElement
+ * CVM-Role:        Utility Function
+ * Maintainer:      Hendrik Erz
+ * License:         GNU GPL v3
+ *
+ * Description:     This function can check if you can render a preview element
+ *                  at the provided position
+ *
+ * END HEADER
+ */
+
 import { Editor, Position } from 'codemirror'
 
+/**
+ * Given the provided positions, this function returns true if you are free to
+ * render an element there
+ *
+ * @param   {Editor}    cm    The editor instance
+ * @param   {Position}  from  The beginning position
+ * @param   {Position}  to    The ending position
+ *
+ * @return  {boolean}         Returns true if you can render an element there
+ */
 export default function canRenderElement (cm: Editor, from: Position, to: Position): boolean {
   // Check if the cursor is within the range
   const cursor = cm.getCursor('head')
