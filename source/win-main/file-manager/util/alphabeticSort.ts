b346017524f664a8ed4f2c235a4ded826b27e65d
Spencer <a1765159@adelaide.edu.au>
2022-05-16 14:22:33 +0930

    added docmentation and addressed linting

--- source/win-main/file-manager/util/alphabeticSort.ts
+++ source/win-main/file-manager/util/alphabeticSort.ts
@@ -1,31 +1,56 @@
-import { MDFileMeta, CodeFileMeta, DirMeta } from '@dts/common/fsal'
+/**
+ * @ignore
+ * BEGIN HEADER
+ *
+ * Contains:        alphabeticSort
+ * CVM-Role:        Utility Function
+ * Maintainer:      
+ * License:         GNU GPL v3
+ *
+ * Description:     A utility function that sorts a given array of directories.
+ *                  The function should be passed the array and a string determining
+ *                  how to sort the array.
+ *
+ * END HEADER
+ */
 
-export default function alphabeticSort(dirArray: any, sortBy: string): DirMeta[] {
-    function compare(a: any, b: any) {
+import { DirMeta } from '@dts/common/fsal'
 
-      let nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase();
+/**
+ * Returns a sorted array of directories
+ *
+ * @param   {any}      dirArray         The directories to be sorted
+ * @param   {string}   sortBy           How to sort the directories
+ *
+ * @return  {DirMeta[]}                 The sorted array
+ */
+export default function alphabeticSort (dirArray: DirMeta[], sortBy: string): DirMeta[] {
+  function compare (a: DirMeta, b: DirMeta): number {
 
-      // if workspaces have the same name, compare their parent's name
-      if (nameA == nameB) {
-        let dirA = a.dir.toLowerCase(), dirB = b.dir.toLowerCase();
-        if (dirA < dirB)
-          return -1;
-        if (dirA > dirB)
-          return 1;
-      }
-             
-      // sort by workspace's name
-      else {
-        if (nameA < nameB) {
-          return -1
-        }
-        if (nameA > nameB) {
-          return 1
-        }
-      }
-      return 0;
+    // get names of directory in lowercase due to ASCII values
+    let nameA = a.name.toLowerCase()
+    let nameB = b.name.toLowerCase()
+
+    // if directories have the same name, compare their parent's name
+    if (nameA === nameB) {
+      let dirA = a.dir.toLowerCase()
+      let dirB = b.dir.toLowerCase()
+      if (dirA < dirB) { return -1 }
+      if (dirA > dirB) { return 1 }
+
+    // else compare regularly
+    } else {
+      if (nameA < nameB) { return -1 }
+      if (nameA > nameB) { return 1 }
     }
-    
-    if (sortBy == "desc") return dirArray.sort(compare).reverse() as DirMeta[];
-    return dirArray.sort(compare) as DirMeta[];
+    return 0
   }
+  
+  // return array in reverse alphabetical order
+  if (sortBy === 'desc') {
+    return dirArray.sort(compare).reverse()
+  }
+
+  // return array in alphabetical order
+  return dirArray.sort(compare)
+}
