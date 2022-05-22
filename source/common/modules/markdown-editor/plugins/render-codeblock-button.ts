089bb9d1230edfa469dc307646c590b923c0529f
sxyxs <xiangyushi7@gmail.com>
2022-05-22 18:14:10 +0930

    stuck in read global codeblock-open, switch labtop

--- source/common/modules/markdown-editor/plugins/render-codeblock-button.ts
+++ source/common/modules/markdown-editor/plugins/render-codeblock-button.ts
@@ -106,10 +106,11 @@
 
  import CodeMirror, { commands } from 'codemirror'
  import canRenderElement from './util/can-render-element'
- 
+//import count_codeblock from 'source/common/modules/markdown-editor/hooks/codeblock-classes'
+
  // const emphasisRE = /(?<![\\])(?<=\s|^)([*_]{1,3}|~{2})((?!\s)[^*_]+?(?<![\s\\]))(?:[*_]{1,3}|~{2})(?=\s|$)/g
  const emphasisRE = /(?<![\\\w])([*_]{1,3}|~{2})((?!\s)[^*_]+?(?![\s\\]))(?:[*_]{1,3}|~{2})(?![\\\w])/g
- 
+
  /**
   * Declare the markdownRenderEmphasis command
   *
@@ -122,20 +123,20 @@
      if (cm.getModeAt({ line: i, ch: 0 }).name !== 'markdown-zkn') {
        continue
      }
- 
+
      // First get the line and test if the contents contain a link
      const line = cm.getLine(i)
- 
+
      for (const match of line.matchAll(emphasisRE)) {
        const which = match[1].length // 1=italic, 2=bold, 3=italic and bold
        // Since we're using matchAll, index will not be undefined
        const from = { line: i, ch: match.index as number }
        const to = { line: i, ch: match.index as number + match[0].length }
- 
+
        if (!canRenderElement(cm, from, to)) {
          continue
        }
- 
+
        const span = document.createElement('span')
        switch (which) {
          case 1: // Italic
@@ -152,16 +153,16 @@
            span.style.fontStyle = 'italic'
            span.style.fontWeight = 'bold'
            break
-       }	
+       }
        span.style.background = 'green'
- 
+
        // Replace the few things that can be part of an emphasis
        let contents = match[2]
        contents = contents.replace(/~~([^~]+?)~~/g, '<del>$1</del>')
        contents = contents.replace(/`([^`]+?)`/g, '<code>$1</code>')
- 
+
        span.innerHTML = contents
- 
+
        const textMarker = cm.markText(
          from, to,
          {
@@ -172,32 +173,51 @@
            inclusiveRight: false
          }
        )
- 
+
        span.onclick = function (e) {
          textMarker.clear()
          cm.setCursor(cm.coordsChar({ left: e.clientX, top: e.clientY }))
          cm.focus()
        }
      }
-     
+
      // -------------- CODE TO CREATE BUTTON -------------------------------
      // At the moment this only gets the first code block (the array[0] part)
      // This can be fixed later by adding a for loop over all the elements with
      // the class name
-     let codeBlock = document.getElementsByClassName("code-block-first-line")[0]
-     
+     //export default function copy_button (var count_position): void {
+     //}
+     var count_codeblock = 0
+     const codeBlockRE = /^(?:\s{0,3}`{3}|~{3}).*/
+     const lineCount = cm.lineCount()
+     for (let j = 0; j<lineCount; j++){
+       const line = cm.getLine(i)
+       if (codeBlockRE.test(line)){
+         //console.log(cm.getLine(++i))
+         count_codeblock = count_codeblock + 1
+       }
+     }
+    // console.log(count_codeblock,"count_codeblock")
+
+
+     //console.log("count block",count_codeblock)
+     //for (let i = 0; i < count_codeblock; i++) {
+
+     //}
+     let codeBlock = document.getElementsByClassName("code-block-first-line")[1]
+
      // Create a button
      let copyButton = document.createElement("button")
      copyButton.className = "code-block-copy-button"
      copyButton.innerText = "Copy Code Block"
-     
+
      // If the code block does not already have a button
      // I added this because it seemed to be constantly adding buttons while
      // the render plugin was running
      // This way, only one button will be added to a code block
      if (codeBlock.querySelector(".code-block-copy-button") == null) {
-     	codeBlock.appendChild(copyButton)
+       codeBlock.appendChild(copyButton)
      }
    } // END for-loop
  }
- 
+
