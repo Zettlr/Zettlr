b9cd04a5cf5fdf461f10e9715979d1ae13b384d7
Felix Nüsse <felix.nuesse@t-online.de>
2022-06-19 12:31:09 +0200

    add more documentation to functions
    Signed-off-by: Felix Nüsse <felix.nuesse@t-online.de>

--- source/app/service-providers/cli-provider.ts
+++ source/app/service-providers/cli-provider.ts
@@ -22,6 +22,22 @@ export const DISABLE_HARDWARE_ACCELERATION = 'disable-hardware-acceleration'
 export const CLEAR_CACHE = 'clear-cache'
 export const LAUNCH_MINIMIZED = 'launch-minimized'
 
+/**
+ * This function returns wether or not a predefined argument was passed to the zettlr executable.
+ * Possible keys:
+ *    data-dir,
+ *    disable-hardware-acceleration,
+ *    clear-cache,
+ *    launch-minimized
+ *
+ * Please use the defined constants instead of raw values!
+ *
+ * @param key  {string}   This is the key to be checked.
+ *
+ * @return  {string}      If the key is of the format -x=y, --xlong=y, a string with y is returned (If it was predefined)
+ * @return  {boolean}     If the key is of the format -x, --xlong, a boolean is returned. True if it was passed, false if not  (If it was predefined)
+ * @return  {undefined}   If the key is not predefined, this function returns undefined
+ */
 export function getCLIArgument (key: string): string | boolean | undefined {
   switch (key) {
     case DATA_DIR: {
@@ -71,6 +87,9 @@ export function handleGeneralArguments (): void {
   }
 }
 
+/**
+ * Print a small CLI help on stdout.
+ */
 function showHelp (): void {
   console.log('This is the Zettlr Help!')
   console.log('')
@@ -83,6 +102,16 @@ function showHelp (): void {
   console.log('-m | --launch-minimized                Start Zettlr mimimized in the Systemtray')
 }
 
+/**
+ * This allows to get a raw, unprocessed value for any argument passed via ClI.
+ * The passed argument has to be either of the format -x=y or --xlong=y.
+ *
+ * @param key  {string}   This is the key to be checked. OMIT the equals sign, eg. -x or --xlong; NOT -x=y.
+ *
+ *
+ * @return  {string}      If the key is of the format -x=y, --xlong=y, a string with y is returned
+ * @return  {undefined}   If the key was not passed or is not of the format specified in the description.
+ */
 export function getArgumentValue (key: string): string | undefined {
   const argument = process.argv.find(elem => elem.indexOf(key + '=') === 0)
   if (argument === undefined) {
