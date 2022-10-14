f50a30999d683ba8ea12d8ba1683478bf35f6053
Hendrik Erz <hendrik@zettlr.com>
2022-10-14 16:39:55 +0200

    fix: Detect standalone links

--- source/common/modules/markdown-editor/parser/syntax-extensions.ts
+++ source/common/modules/markdown-editor/parser/syntax-extensions.ts
@@ -18,21 +18,26 @@ function getInlineDecorator (): Extension {
   const citationDeco = Decoration.mark({ class: 'cm-citation' })
   const tagDeco = Decoration.mark({ class: 'cm-zkn-tag' })
   const highlightDeco = Decoration.mark({ class: 'cm-highlight' })
-  const headingDeco = Decoration.mark({ class: 'cm-formatting-header' })
+  const urlDeco = Decoration.mark({ class: 'cm-link' })
+
+  const citationRe = /(@\w+)/.source
+  const highlightRe = /(::.+?::)/.source
+  const tagRe = /(?<=^|\s|[({[])#(#?[^\s,.:;…!?"'`»«“”‘’—–@$%&*#^+~÷\\/|<=>[\](){}]+#?)/.source
+  const urlRe = /\[([^\]]+)\]\((.+?)\)|(((?:(?:aaas?|about|acap|adiumxtra|af[ps]|aim|apt|attachment|aw|beshare|bitcoin|bolo|callto|cap|chrome(?:-extension)?|cid|coap|com-eventbrite-attendee|content|crid|cvs|data|dav|dict|dlna-(?:playcontainer|playsingle)|dns|doi|dtn|dvb|ed2k|facetime|feed|file|finger|fish|ftp|geo|gg|git|gizmoproject|go|gopher|gtalk|h323|hcp|https?|iax|icap|icon|im|imap|info|ipn|ipp|irc[6s]?|iris(?:\.beep|\.lwz|\.xpc|\.xpcs)?|itms|jar|javascript|jms|keyparc|lastfm|ldaps?|magnet|mailto|maps|market|message|mid|mms|ms-help|msnim|msrps?|mtqp|mumble|mupdate|mvn|news|nfs|nih?|nntp|notes|oid|opaquelocktoken|palm|paparazzi|platform|pop|pres|proxy|psyc|query|res(?:ource)?|rmi|rsync|rtmp|rtsp|secondlife|service|session|sftp|sgn|shttp|sieve|sips?|skype|sm[bs]|snmp|soap\.beeps?|soldat|spotify|ssh|steam|svn|tag|teamspeak|tel(?:net)?|tftp|things|thismessage|tip|tn3270|tv|udp|unreal|urn|ut2004|vemmi|ventrilo|view-source|webcal|wss?|wtai|wyciwyg|xcon(?:-userid)?|xfire|xmlrpc\.beeps?|xmpp|xri|ymsgr|z39\.50[rs]?):(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.-]+[.][a-z]{2,4}\/)(?:[^\s()<>]|\([^\s()<>]*\))+(?:\([^\s()<>]*\)|[^\s`*!()[\]{};:'".,<>?«»“”‘’])))|([a-z0-9.\-_+]+?@[a-z0-9.\-_+]+\.[a-z]{2,7})/.source
 
   // Next, we need a so-called MatchDecorator that parses the code and returns a
   // decoration that should apply to those matches
   const decorator = new MatchDecorator({
-    regexp: /(@\w+)|(::.*?::)|(^#{1,6}\s+)|(?<=^|\s|[({[])#(#?[^\s,.:;…!?"'`»«“”‘’—–@$%&*#^+~÷\\/|<=>[\](){}]+#?)/ug,
+    regexp: new RegExp(`${citationRe}|${highlightRe}|${tagRe}|${urlRe}`, 'gu'),
     decoration: m => {
       if (m[1] !== undefined) {
         return citationDeco
       } else if (m[2] !== undefined) {
         return highlightDeco
       } else if (m[3] !== undefined) {
-        return headingDeco
-      } else {
         return tagDeco
+      } else {
+        return urlDeco
       }
     }
   })
