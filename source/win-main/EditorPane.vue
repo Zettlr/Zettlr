e6efb09edad10c40236f2a92a13ba781f65e3391
Hendrik Erz <hendrik@zettlr.com>
2022-11-04 12:06:58 +0100

    fix: Make editor readonly with no open docs Fixes #3854

--- source/win-main/EditorPane.vue
+++ source/win-main/EditorPane.vue
@@ -12,6 +12,7 @@
       v-bind:window-id="windowId"
     ></DocumentTabs>
     <MainEditor
+      v-if="activeFile !== null"
       ref="editor"
       v-bind:distraction-free="distractionFree"
       v-bind:leaf-id="leafId"
@@ -19,11 +20,13 @@
       v-bind:editor-commands="editorCommands"
       v-on:global-search="$emit('globalSearch', $event)"
     ></MainEditor>
+    <div v-else class="empty-pane"></div>
   </div>
   <!-- A single editor pane can either be a pane itself OR a MultiSplitView -->
 </template>
 
 <script lang="ts">
+import { LeafNodeJSON, OpenDocument } from '@dts/common/documents'
 import { EditorCommands } from '@dts/renderer/editor'
 import { defineComponent } from 'vue'
 import DocumentTabs from './DocumentTabs.vue'
@@ -71,16 +74,39 @@ export default defineComponent({
     },
     distractionFree () {
       return this.$store.state.distractionFreeMode === this.leafId
+    },
+    node (): LeafNodeJSON|undefined {
+      return this.$store.state.paneData.find((leaf: LeafNodeJSON) => leaf.id === this.leafId)
+    },
+    activeFile (): OpenDocument|null {
+      return this.node?.activeFile ?? null
     }
   }
 })
 </script>
 
 <style lang="less">
-body .split-pane-container {
-  .editor-pane {
-    // Styles for the editor pane
-    flex-grow: 1;
+body {
+  .split-pane-container {
+    .editor-pane {
+      // Styles for the editor pane
+      flex-grow: 1;
+    }
+  }
+
+  .editor-pane .empty-pane {
+    position: absolute;
+    top: 30px; // Space for the document tabbar
+    bottom: 0;
+    left: 0;
+    right: 0;
+    // If the editor is empty, display a nice background image
+    background-position: center center;
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-color: inherit;
+    background-image: url(../common/img/logo.svg);
+    padding-top: 5em;
   }
 }
 </style>
