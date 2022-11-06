4acbeaed7aa17c0cff41f2878bbba7f4184f7c16
Hendrik Erz <hendrik@zettlr.com>
2022-11-06 11:18:05 +0100

    fix: Increase plain link detection speed

--- source/common/modules/markdown-editor/parser/plain-link-parser.ts
+++ source/common/modules/markdown-editor/parser/plain-link-parser.ts
@@ -15,20 +15,28 @@
 
 import { InlineParser } from '@lezer/markdown'
 
+// NOTE: The original URL regexp from the old Markdown mode may have looked
+// sophisticated, but for our purposes here it's way too slow to compute.
+// Instead of being exact, we're using heuristics here. If a slice of text looks
+// even remotely like a link, we're gonna assume it's a link. Namely, we're
+// assuming a link if something starts with a protocol (https://) or if it
+// follows the form www.google.com (+ optional port number)
+const protocolRe = /^[a-z0-9-]+:\/{1,3}[^\s\W]/i
+const wwwRe = /^[a-z0-9-]+\.[a-z0-9-]+\.[a-z]{2,}(?::\d{2,5})?/i
+
 // A small parser that can parse plain text links
 export const plainLinkParser: InlineParser = {
   name: 'plain-links',
   parse: (ctx, next, pos) => {
-    const urlRe = /\[([^\]]+)\]\((.+?)\)|(((?:(?:aaas?|about|acap|adiumxtra|af[ps]|aim|apt|attachment|aw|beshare|bitcoin|bolo|callto|cap|chrome(?:-extension)?|cid|coap|com-eventbrite-attendee|content|crid|cvs|data|dav|dict|dlna-(?:playcontainer|playsingle)|dns|doi|dtn|dvb|ed2k|facetime|feed|file|finger|fish|ftp|geo|gg|git|gizmoproject|go|gopher|gtalk|h323|hcp|https?|iax|icap|icon|im|imap|info|ipn|ipp|irc[6s]?|iris(?:\.beep|\.lwz|\.xpc|\.xpcs)?|itms|jar|javascript|jms|keyparc|lastfm|ldaps?|magnet|mailto|maps|market|message|mid|mms|ms-help|msnim|msrps?|mtqp|mumble|mupdate|mvn|news|nfs|nih?|nntp|notes|oid|opaquelocktoken|palm|paparazzi|platform|pop|pres|proxy|psyc|query|res(?:ource)?|rmi|rsync|rtmp|rtsp|secondlife|service|session|sftp|sgn|shttp|sieve|sips?|skype|sm[bs]|snmp|soap\.beeps?|soldat|spotify|ssh|steam|svn|tag|teamspeak|tel(?:net)?|tftp|things|thismessage|tip|tn3270|tv|udp|unreal|urn|ut2004|vemmi|ventrilo|view-source|webcal|wss?|wtai|wyciwyg|xcon(?:-userid)?|xfire|xmlrpc\.beeps?|xmpp|xri|ymsgr|z39\.50[rs]?):(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.-]+[.][a-z]{2,4}\/)(?:[^\s()<>]|\([^\s()<>]*\))+(?:\([^\s()<>]*\)|[^\s`*!()[\]{};:'".,<>?«»“”‘’])))|([a-z0-9.\-_+]+?@[a-z0-9.\-_+]+\.[a-z]{2,7})/
     const relativeOffset = pos - ctx.offset
-    const match = urlRe.exec(ctx.text.slice(relativeOffset))
+    const slice = ctx.text.slice(relativeOffset)
+    const nextSpace = slice.includes(' ') ? slice.indexOf(' ') : ctx.end - pos
 
-    if (match === null || match.index > 0) {
+    if (protocolRe.test(slice) || wwwRe.test(slice)) {
+      return ctx.addElement(ctx.elt('URL', pos, pos + nextSpace))
+    } else {
+      // Definitely not a link
       return -1
     }
-
-    // At this point we have a plain text link (NOTE its node type is URL as is
-    // for "regular" Markdown links: [Link](Url))
-    return ctx.addElement(ctx.elt('URL', pos, pos + match[0].length))
   }
 }
