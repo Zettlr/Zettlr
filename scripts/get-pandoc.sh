ae170395d7d61e0bc951f36818d73e56882ed6ff
Hendrik Erz <hendrik@zettlr.com>
2023-03-16 11:29:02 +0100

    chore: Bump Pandoc to v3.1.1

--- scripts/get-pandoc.sh
+++ scripts/get-pandoc.sh
@@ -5,7 +5,7 @@
 # because this gives us more control over the built-in pandoc binary.
 
 # Retrieve the versions from https://github.com/jgm/pandoc/releases/latest
-VERSION="2.19.2"
+VERSION="3.1.1"
 
 BASE_PATH="https://github.com/jgm/pandoc/releases/download/$VERSION"
 
