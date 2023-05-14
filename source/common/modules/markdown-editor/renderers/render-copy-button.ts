ec2e7b86c13fc2c777e859f2c45f8dc92b7bf268
hpretila <grigrass@live.com>
2023-05-14 17:11:51 +0930

    feat: CSS Hell+clickAndSelect

--- source/common/modules/markdown-editor/renderers/render-copy-button.ts
+++ source/common/modules/markdown-editor/renderers/render-copy-button.ts
@@ -12,10 +12,13 @@
  * END HEADER
  */
 
+import clickAndSelect from './click-and-select'
 import { renderBlockWidgets } from './base-renderer'
 import { SyntaxNodeRef, SyntaxNode } from '@lezer/common'
 import { EditorView, WidgetType } from '@codemirror/view'
 import { EditorState } from '@codemirror/state'
+import { sleep } from '@cds/core/internal'
+import editor from 'source/win-preferences/schema/editor'
 
 class CopyButtonWidget extends WidgetType {
   constructor (readonly nodeContents: string, readonly node: SyntaxNode) {
@@ -28,17 +31,40 @@ class CopyButtonWidget extends WidgetType {
   }
 
   toDOM (view: EditorView): HTMLElement {
-    const elem = document.createElement('span')
-    let text = elem.appendChild(document.createElement('span'))
-    text.textContent = this.nodeContents
-    let copyButton = elem.appendChild(document.createElement('button'))
-    copyButton.innerText = 'copy'
+    const elem = document.createElement('div')
+    elem.style.width = '100%'
+    elem.style.position = 'relative'
 
-    copyButton.addEventListener('click', (event) => {
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises
-      navigator.clipboard.writeText(text.textContent !== null ? text.textContent : '')
+    let copyButton = elem.appendChild(document.createElement('button'))
+    copyButton.innerText = 'Copy'
+    copyButton.style.top = '8px'
+    copyButton.style.right = '8px'
+    copyButton.style.zIndex = '10'
+    copyButton.style.position = 'absolute'
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    copyButton.addEventListener('click', async (event) => {
+      event.preventDefault()
+      event.stopPropagation()
+      await navigator.clipboard.writeText(this.nodeContents !== null ? this.nodeContents : '')
+      copyButton.innerText = 'Copied!'
+      await sleep(500)
+      copyButton.innerText = 'Copy'
     })
+
+    let text = elem.appendChild(document.createElement('span'))
+    text.style.padding = '0px'
+    text.style.margin = '0px'
+    text.style.lineBreak = '0px'
+    text.style.width = '100%'
+    text.style.display = 'block'
+    text.textContent = this.nodeContents
+    text.addEventListener('click', clickAndSelect(view, this.node))
+
     return elem
+  }
+
+  ignoreEvent (event: Event): boolean {
+    return true // !(event.target instanceof HTMLElement) // By default ignore all events
   }
 }
 
