830ecff47d7605493667df740cb1afdab55d88db
Mauricio Andrade <guzmandrade@gmail.com>
2023-08-21 21:17:33 -0500

    added separated class for inline code

--- source/common/modules/markdown-editor/plugins/code-background.ts
+++ source/common/modules/markdown-editor/plugins/code-background.ts
@@ -2,7 +2,7 @@ import { syntaxTree } from '@codemirror/language'
 import { EditorSelection } from '@codemirror/state'
 import { layer, RectangleMarker } from '@codemirror/view'
 
-export const codeBackground = layer({
+export const codeblockBackground = layer({
   above: false, // Render below text
   class: 'cm-codeBackgroundLayer',
   update (update, layer) {
@@ -18,22 +18,57 @@ export const codeBackground = layer({
       enter: (node) => {
         // CodeText contains a single node that has all the code's contents
         // We also want InlineCode to be handled here.
-        if (node.type.name !== 'CodeText' && node.type.name !== 'InlineCode') {
+        if (node.type.name !== 'CodeText') {
           return
-        }
-        // Default config for inline code
-        let from = node.from + 1
-        let to = node.to - 1
-        if (node.type.name === 'CodeText') {
-          // Branch for Code Block
-          from = node.from
-          to = node.to + 1 // Necessary to draw a background behind the entire last line
         }
         try {
           const localMarkers = RectangleMarker.forRange(
             view,
-            (node.type.name === 'CodeText') ? 'code code-line-background' : 'code inline-code-background',
-            EditorSelection.range(from, to)
+            'code code-block-line-background',
+            EditorSelection.range(node.from, node.to + 1)
+          )
+
+          markers.push(...localMarkers)
+        } catch (err: any) {
+          // Sometimes, the RectangleMarker throws an error because it "cannot
+          // read properties of null (reading 'top')". The reason seems to be
+          // that the corresponding line DOM objects aren't drawn when the
+          // plugin attempts to draw the rectangle marker. This is noticeable in
+          // a slight flicker of the background. However, to me it seems
+          // negligible, and hence we're just swallowing the error.
+        }
+        return false
+      }
+    })
+    // Third, return
+    return markers
+  }
+})
+
+export const inlineCodeBackground = layer({
+  above: false, // Render below text
+  class: 'cm-inlineCodeBackgroundLayer',
+  update (update, layer) {
+    return update.docChanged || update.viewportChanged // Return true to redraw markers
+  },
+  markers (view) {
+    // First, collect all code blocks
+    const markers: RectangleMarker[] = []
+    // Second, create RectangleMarkers for each of them
+    syntaxTree(view.state).iterate({
+      from: 0,
+      to: view.state.doc.length,
+      enter: (node) => {
+        // CodeText contains a single node that has all the code's contents
+        // We also want InlineCode to be handled here.
+        if (node.type.name !== 'InlineCode') {
+          return
+        }
+        try {
+          const localMarkers = RectangleMarker.forRange(
+            view,
+            'code inline-code-background',
+            EditorSelection.range(node.from + 1, node.to - 1)
           )
 
           markers.push(...localMarkers)
