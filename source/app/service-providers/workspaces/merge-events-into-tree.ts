fd1be9c0649f2d48b9b2fcdbddcbac2ca3e84885
Hendrik Erz <hendrik@zettlr.com>
2023-11-11 18:06:52 +0100

    feat: Move out mergeEventsIntoTree to make it available in main and renderer

---
+++ source/app/service-providers/workspaces/merge-events-into-tree.ts
@@ -0,0 +1,68 @@
+
+import locateByPath from '@providers/fsal/util/locate-by-path'
+import type { AnyDescriptor, DirDescriptor } from '@dts/common/fsal'
+import type { ChangeDescriptor } from './root'
+
+const PATH_SEP = process.platform === 'win32' ? '\\' : '/'
+
+/**
+ * This function takes a series of change events for the file trees and merges
+ * those one after another into the provided tree, modifying it in place. NOTE:
+ * This requires that the events are actually accumulated for this tree;
+ * providing another tree will lead to errors and inconsistencies. NOTE: This
+ * function will also be imported by the renderer so DO NOT USE ANY MAIN PROCESS
+ * DEPENDENCIES!
+ *
+ * @param   {ChangeDescriptor[]}  events  The list of changes
+ * @param   {AnyDescriptor}       tree    The tree to merge the changes into
+ */
+export function mergeEventsIntoTree (events: ChangeDescriptor[], tree: AnyDescriptor): void {
+  for (const event of events) {
+    if (event.type === 'add') {
+      // Find the parent, and add the given descriptor to its children
+      const parent = locateByPath(tree, event.descriptor.dir)
+      if (parent === undefined || parent.type !== 'directory') {
+        throw new Error('Received an add event, but the tree descriptor did not contain its parent!')
+      }
+
+      // TODO: Sort the parent properly!!!
+      parent.children.push(event.descriptor)
+    } else if (event.type === 'change') {
+      const parent = locateByPath(tree, event.descriptor.dir)
+
+      if (parent === undefined || parent.type !== 'directory') {
+        throw new Error('Received a change event, but the tree descriptor did not contain its parent!')
+      }
+
+      const idx = parent.children.findIndex(desc => desc.path === event.path)
+
+      if (idx < 0) {
+        throw new Error('Received a change event but could not find the old descriptor in the parent!')
+      }
+
+      if (event.descriptor.type === 'directory') {
+        // Ensure to carry over the recursive children array
+        event.descriptor.children = (parent.children[idx] as DirDescriptor).children
+      }
+
+      parent.children.splice(idx, 1, event.descriptor)
+    } else {
+      // Unlink event
+      // NOTE: We cannot use path here since this function is also required in
+      // renderer processes
+      const dirname = event.path.substring(0, event.path.lastIndexOf(PATH_SEP))
+      const parent = locateByPath(tree, dirname)
+      if (parent === undefined || parent.type !== 'directory') {
+        throw new Error('Received an unlink event but could not find its parent!')
+      }
+
+      const idx = parent.children.findIndex(desc => desc.path === event.path)
+
+      if (idx < 0) {
+        throw new Error('Could not remove descriptor from tree!')
+      }
+
+      parent.children.splice(idx, 1)
+    }
+  }
+}
