ab0ae5cdd108094a0787c8f0396d5495beff5367
Kirthihan Yasotharan <kirthihan.y@gmail.com>
2023-11-24 13:28:57 +0100

    fix: Handle Zkn link in Markdown to HTML conversion (#4601)

--- source/common/modules/markdown-utils/markdown-to-html.ts
+++ source/common/modules/markdown-utils/markdown-to-html.ts
@@ -364,6 +364,8 @@ function nodeToHTML (node: ASTNode|ASTNode[], getCitation: (citations: CiteItem[
     const close = tagInfo.selfClosing ? '' : `</${tagInfo.tagName}>`
     const body = tagInfo.selfClosing ? '' : nodeToHTML(node.children, getCitation)
     return `${open}${body}${close}`
+  } else if (node.type === 'ZettelkastenLink') {
+    return `[[${node.value}]]`
   }
 
   return ''
