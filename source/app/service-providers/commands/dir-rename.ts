036b3ab6bf7ff5bd77e9b25291559f70dec06600
Hendrik Erz <hendrik@zettlr.com>
2023-11-25 16:04:44 +0100

    fix: Dir renaming

--- source/app/service-providers/commands/dir-rename.ts
+++ source/app/service-providers/commands/dir-rename.ts
@@ -39,11 +39,24 @@ export default class DirRename extends ZettlrCommand {
     // At this point no file is open in that directory anymore, so we can easily
     // rename the directory. The FSAL will reflect the changes.
     try {
+      // Before renaming the dir, let's see if it is a workspace. Because if it
+      // is, we have to close it first.
+      const { openPaths } = this._app.config.getConfig()
+      const isRoot = openPaths.includes(sourceDir.path)
+
+      if (isRoot) {
+        this._app.config.removePath(sourceDir.path)
+      }
+
       await this._app.fsal.rename(arg.path, newPath)
       // Notify the documents provider so it can exchange any files if necessary
       await this._app.documents.hasMovedDir(arg.path, newPath)
+
+      if (isRoot) {
+        this._app.config.addPath(newPath)
+      }
     } catch (err: any) {
-      console.error(err)
+      this._app.log.error(`Error during renaming file: ${err.message as string}`, err)
       this._app.windows.prompt({
         type: 'error',
         title: err.name,
