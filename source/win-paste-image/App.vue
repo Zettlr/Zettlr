04b2f4c422c8f60d46b60c3d7b877c7aea213293
Hendrik Erz <hendrik@zettlr.com>
2023-12-23 14:13:15 +0100

    refactor: Fix @typescript-eslint/prefer-nullish-coalescing rule

--- source/win-paste-image/App.vue
+++ source/win-paste-image/App.vue
@@ -122,7 +122,7 @@ export default defineComponent({
       originalHeight: size.height,
       aspectRatio: aspect,
       retainAspect: true,
-      targetPath: (startPath !== null) ? startPath : '',
+      targetPath: startPath ?? '',
       fileName: name
     }
   },
