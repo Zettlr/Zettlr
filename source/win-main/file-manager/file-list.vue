7f2e1b742da77a029fcff5e2d1e43ad47749d921
Hendrik Erz <hendrik@zettlr.com>
2023-12-23 13:53:02 +0100

    refactor: Fix @typescript-eslint/block-spacing rule

--- source/win-main/file-manager/file-list.vue
+++ source/win-main/file-manager/file-list.vue
@@ -158,12 +158,12 @@ export default defineComponent({
         return 30
       }
     },
-    getDirectoryContents: function (): Array<{ id: number, props: MaybeRootDescriptor}> {
+    getDirectoryContents: function (): Array<{ id: number, props: MaybeRootDescriptor }> {
       if (this.$store.state.selectedDirectory === null) {
         return []
       }
 
-      const ret: Array<{ id: number, props: MaybeRootDescriptor}> = []
+      const ret: Array<{ id: number, props: MaybeRootDescriptor }> = []
       const items = objectToArray(this.$store.state.selectedDirectory, 'children') as AnyDescriptor[]
       for (let i = 0; i < items.length; i++) {
         if (items[i].type !== 'other') {
