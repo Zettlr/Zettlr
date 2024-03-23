a54c9b8371b797b761c30d058a54ac8c3f2a78e6
Hendrik Erz <hendrik@zettlr.com>
2024-03-23 15:01:20 +0100

    refactor: Split up markdown AST parser

---
+++ source/common/modules/markdown-utils/ast-utils/generic-text-node.ts
@@ -0,0 +1,31 @@
+/**
+ * @ignore
+ * BEGIN HEADER
+ *
+ * Contains:        genericTextNode
+ * CVM-Role:        Utility
+ * Maintainer:      Hendrik Erz
+ * License:         GNU GPL v3
+ *
+ * Description:     A simple function that wraps a piece of plain text into an
+ *                  ASTNode.
+ *
+ * END HEADER
+ */
+
+import { type TextNode } from '../markdown-ast'
+
+/**
+ * Creates a generic text node; this is used to represent textual contents of
+ * SyntaxNodes.
+ *
+ * @param   {number}    from              The absolute start offset
+ * @param   {number}    to                The absolute end offset
+ * @param   {string}    value             The actual text
+ * @param   {string}    whitespaceBefore  Potential whitespace before the node
+ *
+ * @return  {TextNode}                    The rendered TextNode
+ */
+export function genericTextNode (from: number, to: number, value: string, whitespaceBefore = ''): TextNode {
+  return { type: 'Text', name: 'text', from, to, value, whitespaceBefore }
+}
---
+++ source/common/modules/markdown-utils/ast-utils/get-whitespace-before-node.ts
@@ -0,0 +1,40 @@
+/**
+ * @ignore
+ * BEGIN HEADER
+ *
+ * Contains:        getWhitespaceBeforeNode
+ * CVM-Role:        Utility
+ * Maintainer:      Hendrik Erz
+ * License:         GNU GPL v3
+ *
+ * Description:     Takes a SyntaxNode and returns any whitespace that preceeds
+ *                  the node. This is important since whitespace can be
+ *                  significant; especially when building HTML.
+ *
+ * END HEADER
+ */
+
+import { type SyntaxNode } from '@lezer/common'
+
+/**
+ * Extracts any amount of whitespace (\t\s\n\r\f\v, etc.) that occurs before
+ * this node.
+ *
+ * @param   {SyntaxNode}  node      The node to extract whitespace for
+ * @param   {string}      markdown  The Markdown source to extract the whitespace
+ *
+ * @return  {string}                The whitespace string
+ */
+export function getWhitespaceBeforeNode (node: SyntaxNode, markdown: string): string {
+  if (node.prevSibling !== null) {
+    const sliceBefore = markdown.substring(node.prevSibling.to, node.from)
+    const onlyWhitespace = /(\s*)$/m.exec(sliceBefore) // NOTE the "m" flag
+    return onlyWhitespace !== null ? onlyWhitespace[1] : ''
+  } else if (node.parent !== null) {
+    const sliceBefore = markdown.substring(node.parent.from, node.from)
+    const onlyWhitespace = /(\s*)$/m.exec(sliceBefore) // NOTE the "m" flag
+    return onlyWhitespace !== null ? onlyWhitespace[1] : ''
+  } else {
+    return ''
+  }
+}
