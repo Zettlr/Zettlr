753d6b41794eb014ce9c852440ce7103ef75e94d
Neil Mazumdar <84065477+NMazzy@users.noreply.github.com>
2024-04-28 14:42:41 +0930

    Simplified RemoveLink

--- source/common/modules/markdown-editor/context-menu/link-image-menu.ts
+++ source/common/modules/markdown-editor/context-menu/link-image-menu.ts
@@ -63,12 +63,16 @@ function getURLForNode (node: SyntaxNode, state: EditorState): string|undefined 
 //   }
 // }
 
-// Parses the regex string, I think this might be something that should be moved potentially into the 
+// Parses the regex string, I think this might be something that should be moved potentially into the
 // markdown-editor/util directory potentially
-function removeMarkdownLink (markdownText: string): string {
-  const markdownLinkRegex = /\[([^\]]+)\]\([^)]+\)/g
-  return markdownText.replace(markdownLinkRegex, '$1')
-}
+// function removeMarkdownLink (markdownText: string): string {
+//   const markdownLinkRegex = /\[([^\]]+)\]\([^)]+\)/g
+
+//   // URL Regex from ../../commands/markdown.ts
+//   const urlRE = /^\[([^\]]+)\]\((.+?)\)|(((?:(?:aaas?|about|acap|adiumxtra|af[ps]|aim|apt|attachment|aw|beshare|bitcoin|bolo|callto|cap|chrome(?:-extension)?|cid|coap|com-eventbrite-attendee|content|crid|cvs|data|dav|dict|dlna-(?:playcontainer|playsingle)|dns|doi|dtn|dvb|ed2k|facetime|feed|file|finger|fish|ftp|geo|gg|git|gizmoproject|go|gopher|gtalk|h323|hcp|https?|iax|icap|icon|im|imap|info|ipn|ipp|irc[6s]?|iris(?:\.beep|\.lwz|\.xpc|\.xpcs)?|itms|jar|javascript|jms|keyparc|lastfm|ldaps?|magnet|mailto|maps|market|message|mid|mms|ms-help|msnim|msrps?|mtqp|mumble|mupdate|mvn|news|nfs|nih?|nntp|notes|oid|opaquelocktoken|palm|paparazzi|platform|pop|pres|proxy|psyc|query|res(?:ource)?|rmi|rsync|rtmp|rtsp|secondlife|service|session|sftp|sgn|shttp|sieve|sips?|skype|sm[bs]|snmp|soap\.beeps?|soldat|spotify|ssh|steam|svn|tag|teamspeak|tel(?:net)?|tftp|things|thismessage|tip|tn3270|tv|udp|unreal|urn|ut2004|vemmi|ventrilo|view-source|webcal|wss?|wtai|wyciwyg|xcon(?:-userid)?|xfire|xmlrpc\.beeps?|xmpp|xri|ymsgr|z39\.50[rs]?):(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.-]+[.][a-z]{2,4}\/)(?:[^\s()<>]|\([^\s()<>]*\))+(?:\([^\s()<>]*\)|[^\s`*!()[\]{};:'".,<>?«»“”‘’])))|([a-z0-9.\-_+]+?@[a-z0-9.\-_+]+\.[a-z]{2,7})$/i
+
+//   return markdownText.replace(markdownLinkRegex, '$1')
+// }
 
 /**
  * Shows a context menu appropriate for a link or image using the given node
@@ -85,11 +89,12 @@ export function linkImageMenu (view: EditorView, node: SyntaxNode, coords: { x: 
     console.error('Could not show Link/Image context menu: No URL found!')
     return
   }
-  // Calls the remove markdown thing, although I note this is called on every right link context menu so this might 
-  // not be needed until its clicked, also the node.from is the starting index in the document and node.to is the end 
+  // Calls the remove markdown thing, although I note this is called on every right link context menu so this might
+  // not be needed until its clicked, also the node.from is the starting index in the document and node.to is the end
   // index so when you do the slice doc it gets the full string
-  const textToInsert = removeMarkdownLink(view.state.sliceDoc(node.from, node.to))
+  // const textToInsert = removeMarkdownLink(view.state.sliceDoc(node.from, node.to))
   // const textToInsert = getTextForNode(node, view.state)
+
   const linkTpl: AnyMenuItem[] = [
     {
       id: 'none',
@@ -159,14 +164,17 @@ export function linkImageMenu (view: EditorView, node: SyntaxNode, coords: { x: 
     } else if (clickedID === 'open-img-in-browser') {
       window.location.href = validAbsoluteURI
     } else if (clickedID === 'menu.remove_link') {
-      // Idk if they want us to directly change this stuff here so I think it might be a move basically everything 
-      // I did into the utils directory, we could also go one step further and merge the open markdown links into 
+      // Idk if they want us to directly change this stuff here so I think it might be a move basically everything
+      // I did into the utils directory, we could also go one step further and merge the open markdown links into
       // the utils file as well so all the link utils are together
+      const nodeText = view.state.sliceDoc(node.from, node.to)
+      const linkText = nodeText.match(/\[(.*?)\]/g)?.map(match => match.slice(1, -1))[0]
       view.dispatch({
         changes: {
           from: node.from,
           to: node.to,
-          insert: textToInsert
+          // insert: textToInsert
+          insert: linkText
         }
       })
     }
