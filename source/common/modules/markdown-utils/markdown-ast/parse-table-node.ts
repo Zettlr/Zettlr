0f62c0e3258d6ae15342159ae9df2b8f70df6d69
Hendrik Erz <hendrik@zettlr.com>
2024-07-06 09:52:08 +0200

    fix/refactor: Improve parseTableNode
    I realized Grid tables aren't officially supported, regardless of what the parser does to them

--- source/common/modules/markdown-utils/markdown-ast/parse-table-node.ts
+++ source/common/modules/markdown-utils/markdown-ast/parse-table-node.ts
@@ -22,7 +22,41 @@ import { parseChildren } from './parse-children'
 import { genericTextNode } from './generic-text-node'
 
 /**
- * Parses a SyntaxNode of name "Table"
+ * Creates a new table cell node
+ *
+ * @param   {number}      from      Cell contents start
+ * @param   {number}      to        Cell contents end
+ * @param   {string}      markdown  The full Markdown content (entire document)
+ * @param   {SyntaxNode}  cellNode  An optional table cell node, if there is one
+ *
+ * @return  {TableCell}             The parsed cell
+ */
+function makeTableCell (from: number, to: number, markdown: string, cellNode?: SyntaxNode): TableCell {
+  const textContent = markdown.slice(from, to)
+  const cell: TableCell = {
+    type: 'TableCell',
+    name: 'TableCell',
+    from,
+    to,
+    whitespaceBefore: '',
+    children: [],
+    textContent
+  }
+
+  if (cellNode === undefined) {
+    // Has no real children; here we basically just account for any whitespace
+    cell.children.push(genericTextNode(from, to, textContent))
+  } else {
+    parseChildren(cell, cellNode, markdown)
+  }
+
+  return cell
+}
+
+/**
+ * Parses a SyntaxNode of name "Table". NOTE: Although the parser recognizes
+ * grid tables, these are not officially implemented, and as such will cause
+ * issues.
  *
  * @param   {SyntaxNode}  node      The node to parse
  * @param   {string}      markdown  The original Markdown source
@@ -50,8 +84,8 @@ export function parseTableNode (node: SyntaxNode, markdown: string): Table {
   const headerRow = tableLines.find(line => /^[|+:-]+$/.test(line))
 
   if (headerRow !== undefined) {
-    // Also note down the table type
-    astNode.tableType = headerRow.includes('|') ? 'pipe' : 'grid'
+    // Also note down the table type.
+    astNode.tableType = headerRow.includes('-') ? 'pipe' : 'grid'
     // The plus indicates either a grid table or a special type of pipe table
     // produced by Emacs' orgtbl command
     // (cf. https://pandoc.org/MANUAL.html#extension-pipe_tables)
@@ -81,7 +115,23 @@ export function parseTableNode (node: SyntaxNode, markdown: string): Table {
   } // Else: Couldn't determine either column alignment nor table type
 
   // Now, transform the rows.
+  // NOTE: There are three types of tables we might come across:
+  // 1. Pipes with leading and trailing pipes
+  //    --> Only content rows are reported (not the header line)
+  // 2. Pipes without leading and trailing pipes
+  //    -> Same, but we have no TableDelimiter before and after a row
+  // 3. Grid tables.
+  //    -> Reports all intermediary rows (i.e., separator and content rows) BUT
+  //    grid tables are not officially supported, so support is bad
   for (const row of [ ...header, ...rows ]) {
+    // Ignore grid-table intermediary rows. This essentially converts grid
+    // tables into pipe tables. NOTE: Again reminder that grid tables aren't
+    // supported, so this is just basic reassuring that these tables look at
+    // least somewhat like a table.
+    if (/^[-+=]+$/.test(markdown.slice(row.from, row.to))) {
+      continue
+    }
+
     const rowNode: TableRow = {
       type: 'TableRow',
       name: row.name,
@@ -95,9 +145,10 @@ export function parseTableNode (node: SyntaxNode, markdown: string): Table {
     // NOTE: The Lezer parser intentionally does not emit TableCell nodes for
     // empty cells, so we cannot fully rely on the existence of table cells.
     // See: https://github.com/lezer-parser/markdown/issues/23
-    // Thus, we have to manually move through the lines one by one. If a
-    // TableDelimiter is followed by another TableDelimiter, we know that there
-    // is an empty cell in between.
+    // Thus, we have to manually move through the lines one by one and assume
+    // that between table delimiters there are always cells except for pipes
+    // without leading and trailing pipe characters
+
     let next = row.firstChild
     let wasDelim = false
     while (next !== null) {
@@ -106,32 +157,32 @@ export function parseTableNode (node: SyntaxNode, markdown: string): Table {
       } else if (next.name === 'TableDelimiter' && wasDelim) {
         // Last iteration was a TableDelimiter, and now again --> Empty cell
         const prev = next.prevSibling!
-        const cellNode: TableCell = {
-          type: 'TableCell',
-          name: 'TableCell',
-          from: prev.to,
-          to: next.from,
-          whitespaceBefore: '',
-          // Has no real children; here we basically just account for any whitespace
-          children: [
-            genericTextNode(prev.to, next.from, markdown.slice(prev.to, next.from))
-          ],
-          textContent: markdown.slice(prev.to, next.from)
-        }
-        rowNode.cells.push(cellNode)
+        // Empty cells should be exactly zero in length
+        rowNode.cells.push(
+          makeTableCell(
+            prev.to === next.from ? prev.to : prev.to + 1,
+            prev.to === next.from ? prev.to : prev.to + 1,
+            markdown
+          )
+        )
       } else if (next.name === 'TableCell') {
         // Functional table cell
-        const cellNode: TableCell = {
-          type: 'TableCell',
-          name: 'TableCell',
-          from: next.from,
-          to: next.to,
-          whitespaceBefore: '',
-          children: [],
-          textContent: markdown.slice(next.from, next.to)
+        const cell = makeTableCell(next.from, next.to, markdown, next)
+
+        // Since the Lezer parser caps cell contents with whitespace, but users
+        // may want to add spaces and continue a new word afterwards, here we
+        // allow a functioning table cell to contain one more space after the
+        // cell's boundary. This ensures that users can write naturally inside
+        // table cells.
+        const nextDelim = next.nextSibling
+        if (nextDelim !== null) {
+          const between = markdown.slice(next.to, nextDelim.from)
+          if (/^\s+$/.test(between) && between.length > 1) {
+            cell.to += 1
+            cell.textContent += ' '
+          }
         }
-        parseChildren(cellNode, next, markdown)
-        rowNode.cells.push(cellNode)
+        rowNode.cells.push(cell)
         wasDelim = false
       } else {
         // TODO: There are frequent TableHeader warnings printed to the console.
@@ -145,16 +196,21 @@ export function parseTableNode (node: SyntaxNode, markdown: string): Table {
     // when it comes to grid table parsing and often includes the delimiting
     // pipes between cells as part of the TableCell nodes. Here we account for
     // that and remove that pipe if applicable.
-    for (const cellNode of rowNode.cells) {
-      if (
-        astNode.tableType === 'grid' && cellNode.children.length > 0 &&
-        cellNode.children[0].type === 'Text' && cellNode.children[0].value.startsWith('|')
-      ) {
-        cellNode.children[0].value = cellNode.children[0].value.substring(1)
-        cellNode.children[0].from += 1
-        cellNode.from += 1
+    if (astNode.tableType === 'grid') {
+      for (const cellNode of rowNode.cells) {
+        if (
+          cellNode.children.length > 0 &&
+          cellNode.children[0].type === 'Text' &&
+          cellNode.children[0].value.startsWith('|')
+        ) {
+          cellNode.children[0].value = cellNode.children[0].value.substring(1)
+          cellNode.children[0].from += 1
+          cellNode.textContent = cellNode.textContent.substring(1)
+          cellNode.from += 1
+        }
       }
     }
+
     astNode.rows.push(rowNode)
   }
   return astNode
