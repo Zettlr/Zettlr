83376d98985a8f945b8dad6ed8083fa73ea9fc03
Chip Hogg <charles.r.hogg@gmail.com>
2024-10-26 17:18:58 -0400

    Remove unused import
    This addresses the review feedback in #5273.

--- source/win-main/PopoverTags.vue
+++ source/win-main/PopoverTags.vue
@@ -52,7 +52,7 @@ import PopoverWrapper from './PopoverWrapper.vue'
 import TextControl from '@common/vue/form/elements/TextControl.vue'
 import TabBar, { type TabbarControl } from '@common/vue/TabBar.vue'
 import { trans } from '@common/i18n-renderer'
-import { ref, computed, onBeforeMount, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import { useConfigStore, useTagsStore } from 'source/pinia'
 
 const props = defineProps<{
