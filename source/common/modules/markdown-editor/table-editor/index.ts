35f79a2111ac01cfdb940d190cd21277abc43842
Hendrik Erz <hendrik@zettlr.com>
2024-12-07 17:32:25 +0100

    feat: Initial table styling

--- source/common/modules/markdown-editor/table-editor/index.ts
+++ source/common/modules/markdown-editor/table-editor/index.ts
@@ -46,19 +46,31 @@ export const renderTables = [
   }),
   // A theme for the various elements
   EditorView.baseTheme({
-    '.cm-content .cm-table-editor-widget': {
+    '.cm-table-editor-widget': {
       borderCollapse: 'collapse',
-      margin: '0 2px 0 6px' // Taken from .cm-line so that tables align
+      margin: '0 2px 0 6px', // Taken from .cm-line so that tables align
+      '& td, & th': {
+        color: '#3a3a3a',
+        border: '1px solid #bdbdbd',
+        padding: '2px 4px',
+        '&:hover': { outline: '1px solid #737373' },
+        '&:focus-within': { borderColor: '#1cb27e', outlineColor: '#1cb27e' }
+      },
+      '& tr:first-child td': { backgroundColor: '#dedede' },
+      '& tr:nth-child(2n+3) td': { backgroundColor: '#f7f7f7' }
     },
     '.cm-content .cm-table-editor-widget .cm-scroller': {
       padding: '0' // Override the large margin from the main editor view
     },
-    '.cm-content .cm-table-editor-widget td, .cm-content .cm-table-editor-widget th': {
-      border: '1px solid black',
-      padding: '2px 4px'
-    },
-    '&dark .cm-content .cm-table-editor-widget td, &dark .cm-content .cm-table-editor-widget th': {
-      borderColor: '#aaaaaa'
+    // DARK STYLES
+    '&dark .cm-table-editor-widget': {
+      '& td, & th': {
+        color: '#d6d6d6',
+        borderColor: '#4a4a4a',
+        '&:hover': { outline: '1px solid #949494' }
+      },
+      '& tr:first-child td': { backgroundColor: '#313131' },
+      '& tr:nth-child(2n+3) td': { backgroundColor: '#212121' }
     }
   }),
   subviewUpdatePlugin
