12b65b15a88b6b5ef3ccc2e927d68bc1f9b558f8
Chrislee2023 <chrislyne30@gmail.com>
2025-06-22 16:56:24 +0930

    Fix: Debounce scroll to prevent flickering on input

--- source/common/modules/markdown-editor/editor-extension-sets.ts
+++ source/common/modules/markdown-editor/editor-extension-sets.ts
@@ -32,6 +32,7 @@ import {
   type ViewUpdate,
   type DOMEventHandlers
 } from '@codemirror/view'
+
 import { autocomplete } from './autocomplete'
 import { codeSyntaxHighlighter, markdownSyntaxHighlighter } from './theme/syntax'
 import markdownParser from './parser/markdown-parser'
@@ -75,11 +76,6 @@ import { defaultKeymap } from './keymaps/default'
 import { vimPlugin } from './plugins/vim-mode'
 import { projectInfoField } from './plugins/project-info-field'
 
-/**
- * This interface describes the required properties which the extension sets
- * need to set up the proper extensions necessary for loading the specified
- * documents.
- */
 export interface CoreExtensionOptions {
   initialConfig: EditorConfiguration
   remoteConfig: {
@@ -92,12 +88,6 @@ export interface CoreExtensionOptions {
   domEventsListeners: DOMEventHandlers<any>
 }
 
-/**
- * This compartment is being used to activate/disable the vim or emacs
- * keybindings dependent on the configuration.
- *
- * @var  {Compartment}
- */
 export const inputModeCompartment = new Compartment()
 
 export function getMainEditorThemes (): Record<EditorConfiguration['theme'], { lightThemes: Extension[], darkThemes: Extension[] }> {
@@ -125,28 +115,6 @@ export function getMainEditorThemes (): Record<EditorConfiguration['theme'], { l
   }
 }
 
-/**
- * This private function loads a set of core extensions that are required for
- * all Codemirror instances inside of Zettlr regardless of document type. These
- * include:
- *
- * - Default keymaps
- * - An undo/redo history
- * - Code folding (only the service provider)
- * - A fold gutter
- * - A custom cursor drawing extension
- * - An extension enabling multiple extensions
- * - Search functionality
- * - Tab size and indentation with spaces or tabs
- * - line wrapping
- * - A configuration field (which has some options that only apply to Md or code)
- * - An update listener that is notified on every update
- * - The document authority functions to sync docs remotely
- *
- * @param   {CoreExtensionOptions}  options  The main extension options
- *
- * @return  {Extension[]}                    An array of core extensions
- */
 function getCoreExtensions (options: CoreExtensionOptions): Extension[] {
   const inputMode: Extension[] = []
   if (options.initialConfig.inputMode === 'vim') {
@@ -162,49 +130,44 @@ function getCoreExtensions (options: CoreExtensionOptions): Extension[] {
 
   const themes = getMainEditorThemes()
 
+  let scrollTimer: number | undefined
+
   return [
-    // Both vim and emacs modes need to be included first, before any other
-    // keymap.
     inputModeCompartment.of(inputMode),
-    // Then, include the default keymap
     defaultKeymap(),
     darkMode({ darkMode: options.initialConfig.darkMode, ...themes[options.initialConfig.theme] }),
-    // CODE FOLDING
     codeFolding(),
     foldGutter(),
-    // HISTORY
     history(),
-    // SELECTIONS
-    // Overrides the default browser selection drawing, allows styling
     drawSelection({ drawRangeCursor: false, cursorBlinkRate: 1000 }),
     highlightWhitespace(options.initialConfig.highlightWhitespace),
     dropCursor(),
     EditorState.allowMultipleSelections.of(true),
-    // Ensure the cursor never completely sticks to the top or bottom of the editor
     EditorView.scrollMargins.of(_view => { return { top: 30, bottom: 30 } }),
-    search({ top: true }), // Add a search
-    // TAB SIZES/INDENTATION -> Depend on the configuration field
+    search({ top: true }),
     EditorState.tabSize.from(configField, (val) => val.indentUnit),
     indentUnit.from(configField, (val) => val.indentWithTabs ? '\t' : ' '.repeat(val.indentUnit)),
-    EditorView.lineWrapping, // Enable line wrapping,
+    EditorView.lineWrapping,
     autoCloseBracketsConfig,
-
-    // Allow configuration of the trigger character
     autocompleteTriggerCharacter.of(':'),
-    // TODO: autocompleteTriggerCharacter.from(configField, val => val.FINDANAME),
-
-    // Add the statusbar
     statusbar,
-
-    // Add the configuration and preset it with whatever is in the cached
-    // config.
     configField.init(_state => JSON.parse(JSON.stringify(options.initialConfig))),
 
-    // The updateListener is a custom extension we're using in order to be
-    // able to emit events from this main class based on change events.
-    EditorView.updateListener.of(options.updateListener),
+    EditorView.updateListener.of((update: ViewUpdate) => {
+      if (update.transactions.some(tr => tr.isUserEvent('input'))) {
+        if (scrollTimer !== undefined) {
+          clearTimeout(scrollTimer)
+        }
+        scrollTimer = window.setTimeout(() => {
+          const sel = update.view.state.selection.main
+          const rect = update.view.coordsAtPos(sel.head)
+          if (rect && (rect.top < 0 || rect.bottom > window.innerHeight)) {
+            update.view.scrollDOM.scrollTop += rect.top - 100
+          }
+        }, 50)
+      }
+    }),
 
-    // Enables the editor to fetch updates to the document from main
     hookDocumentAuthority(
       options.remoteConfig.filePath,
       options.remoteConfig.startVersion,
@@ -215,21 +178,6 @@ function getCoreExtensions (options: CoreExtensionOptions): Extension[] {
   ]
 }
 
-/**
- * This private function returns a set of extensions required for all code
- * editors (but not the Markdown editors). These include:
- *
- * - The core extensions
- * - Line numbers
- * - Close-brackets support
- * - Bracket matching (highlighting)
- * - Indent on input (for proper indentation where applicable)
- * - The code syntax highlighter
- *
- * @param   {CoreExtensionOptions}  options  The main extension options
- *
- * @return  {Extension[]}                    An array of generic code extensions
- */
 function getGenericCodeExtensions (options: CoreExtensionOptions): Extension[] {
   return [
     ...getCoreExtensions(options),
@@ -240,35 +188,7 @@ function getGenericCodeExtensions (options: CoreExtensionOptions): Extension[] {
   ]
 }
 
-/**
- * This public function returns a set of extensions required to run a default
- * Zettlr Markdown editor. These include:
- *
- * - The core extensions
- * - The autocompletion functionality
- * - A few extra keys (e.g. for copying as HTML)
- * - The Markdown parser and syntax highlighter
- * - Renderers for the partial WYSIWYG-highlights
- * - A set of extensions to the default syntax of the Codemirror plugin
- * - Document statistics that can be queried
- * - A typewriter mode
- * - A table of contents field
- * - Autocompletion support
- * - A readability mode
- * - The formatting toolbar
- * - Footnote previews on hover
- * - The paste handlers for image saving
- * - The default context menu with default Markdown formats
- * - A spellchecker
- *
- * @param   {CoreExtensionOptions}  options  The main config options
- *
- * @return  {Extension[]}                    An array of Markdown extensions
- */
 export function getMarkdownExtensions (options: CoreExtensionOptions): Extension[] {
-  // The following linters are always active: The spellcheck because that is
-  // turned on and off with the dictionary settings, and the yamlFrontmatterNode
-  // because if that thing has an error, that thing has an error.
   const mdLinterExtensions = [
     spellcheck,
     yamlFrontmatterLint
@@ -282,16 +202,13 @@ export function getMarkdownExtensions (options: CoreExtensionOptions): Extension
   }
 
   if (options.initialConfig.lintLanguageTool) {
-    hasLinters = true // We always add this linter
+    hasLinters = true
   }
 
   if (hasLinters) {
-    // If there's any linter (except the spellchecker), add a lint gutter
     mdLinterExtensions.push(
       lintGutter({
         markerFilter (diagnostics) {
-          // Show any linter warnings and errors in the gutter *except* wrongly
-          // spelled words, since that would be weird.
           return diagnostics.filter(d => d.source !== 'spellcheck' && d.source?.startsWith('language-tool') === false)
         }
       })
@@ -300,52 +217,36 @@ export function getMarkdownExtensions (options: CoreExtensionOptions): Extension
 
   return [
     ...getCoreExtensions(options),
-    // These handlers deal with Markdown specific stuff, for example, pasting
-    // HTML should not add the verbatim HTML code, but rather convert it to
-    // Markdown prior. Additionally, images should get preferential treatment.
     EditorView.domEventHandlers(mdPasteDropHandlers),
-    // We need our custom keymaps first
-    // The parser generates the AST for the document ...
     markdownParser({
       zknLinkParserConfig: { format: options.initialConfig.zknLinkFormat }
     }),
-    // ... which can then be styled with a highlighter
     markdownSyntaxHighlighter(),
-    syntaxExtensions, // Add our own specific syntax plugin
+    syntaxExtensions,
     renderers(options.initialConfig),
     mdLinterExtensions,
     languageTool,
-    // Some statistics we need for Markdown documents
     countField,
     typewriter,
     distractionFree,
     tocField,
     projectInfoField,
-    markdownFolding, // Should be before footnoteGutter
+    markdownFolding,
     autocomplete,
     readabilityMode,
     formattingToolbar,
     footnoteHover,
-    footnoteGutter, // Should be after markdownFolding
+    footnoteGutter,
     urlHover,
     filePreview,
-    backgroundLayers, // Add a background behind inline code and code blocks
-    defaultContextMenu, // A default context menu
-    softwrapVisualIndent, // Always indent visually
-    tagClasses(), // Apply a custom class to each tag so that users can style them (#4589)
+    backgroundLayers,
+    defaultContextMenu,
+    softwrapVisualIndent,
+    tagClasses(),
     EditorView.domEventHandlers(options.domEventsListeners)
   ]
 }
 
-/**
- * This public function returns a set of extensions required to display JSON
- * documents in Zettlr editors. These include the core extensions, the generic
- * code extensions as well as the JSON syntax highlighter.
- *
- * @param   {CoreExtensionOptions}  options  The default options
- *
- * @return  {Extension[]}                    An array of options for JSON files
- */
 export function getJSONExtensions (options: CoreExtensionOptions): Extension[] {
   return [
     ...getGenericCodeExtensions(options),
@@ -355,15 +256,6 @@ export function getJSONExtensions (options: CoreExtensionOptions): Extension[] {
   ]
 }
 
-/**
- * This public function returns a set of extensions required to display YAML
- * documents in Zettlr editors. These include the core extensions, the generic
- * code extensions as well as the YAML syntax highlighter.
- *
- * @param   {CoreExtensionOptions}  options  The default options
- *
- * @return  {Extension[]}                    An array of options for YAML files
- */
 export function getYAMLExtensions (options: CoreExtensionOptions): Extension[] {
   return [
     ...getGenericCodeExtensions(options),
@@ -371,15 +263,6 @@ export function getYAMLExtensions (options: CoreExtensionOptions): Extension[] {
   ]
 }
 
-/**
- * This public function returns a set of extensions required to display LaTeX
- * documents in Zettlr editors. These include the core extensions, the generic
- * code extensions as well as the LaTeX syntax highlighter.
- *
- * @param   {CoreExtensionOptions}  options  The default options
- *
- * @return  {Extension[]}                    An array of options for LaTeX files
- */
 export function getTexExtensions (options: CoreExtensionOptions): Extension[] {
   return [
     ...getGenericCodeExtensions(options),
