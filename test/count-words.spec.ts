cdde3b588c4d2596975bcb1712956567db15986c
benniekiss <63211101+benniekiss@users.noreply.github.com>
2025-09-13 14:50:03 -0400

    add tests

--- test/count-words.spec.ts
+++ test/count-words.spec.ts
@@ -19,61 +19,95 @@ import { strictEqual } from 'assert'
 const countWordsTesters = [
   {
     input: '',
+    locale: 'en',
     expectedWords: 0,
     expectedChars: 0
   },
   {
     input: 'Lorem\n\n# Ipsum',
+    locale: 'en',
     expectedWords: 2,
     expectedChars: 10
   },
   {
     input: 'Lorem\n\n# Ipsum Dolor',
+    locale: 'en',
     expectedWords: 3,
     expectedChars: 15
   },
   {
     input: '\n\n',
+    locale: 'en',
     expectedWords: 0,
     expectedChars: 0
   },
   {
     input: '* one\n* two\n* three',
+    locale: 'en',
     expectedWords: 3,
     expectedChars: 11
   },
   {
     input: '#',
+    locale: 'en',
     expectedWords: 0,
     expectedChars: 0
   },
   {
     input: '---\ntitle: "Some title"\nkeywords:\n  - one\n  - two\n  - three\n...\n\n# Heading\n\nLorem Ipsum dolor, sit amet',
+    locale: 'en',
     expectedWords: 6,
-    expectedChars: 30
+    expectedChars: 29
   },
   {
     input: 'Some text with **bold** and *emphasized* text in __both__ _flavors_ -- including **_mixes_**!',
+    locale: 'en',
     expectedWords: 12,
     expectedChars: 60
   },
   {
     input: 'This is text with a list\n- one\n- two\n- three',
+    locale: 'en',
     expectedWords: 9,
     expectedChars: 30
+  },
+  {
+    input: '我輩は猫である。',
+    locale: 'ja',
+    expectedWords: 5,
+    expectedChars: 7
+  },
+  {
+    input: 'ゼットルは知識をつなぐ。',
+    locale: 'ja',
+    expectedWords: 5,
+    expectedChars: 11
+  },
+  {
+    input: 'Zettlrはゼットルと読む。',
+    locale: 'ja',
+    expectedWords: 5,
+    expectedChars: 14
+  },
+  { input: '**Zettlr**では*Markdown*を使います。',
+    locale: 'ja',
+    expectedWords: 6,
+    expectedChars: 21
   }
 ]
 
 describe('Utility#countWords()', function () {
+  let idx = 0
   for (let test of countWordsTesters) {
-    it(`should return ${test.expectedWords} words`, function () {
+    idx++
+    it(`${idx}. should return ${test.expectedWords} words (${test.locale})`, function () {
       const ast = markdownToAST(test.input)
-      strictEqual(countWords(ast), test.expectedWords)
+      strictEqual(countWords(ast, undefined, undefined, test.locale), test.expectedWords)
     })
 
-    it(`should return ${test.expectedChars} characters`, function () {
+    it(`${idx}. should return ${test.expectedChars} characters (${test.locale})`, function () {
       const ast = markdownToAST(test.input)
-      strictEqual(countChars(ast), test.expectedChars)
+      strictEqual(countChars(ast, undefined, undefined, test.locale), test.expectedChars)
     })
   }
 })
