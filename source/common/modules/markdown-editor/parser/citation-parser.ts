91039915f183ca6ec770544cc09a4c93ce88afe0
Hendrik Erz <hendrik@zettlr.com>
2025-09-17 11:16:03 +0200

    fix: Improvements to the citation parser
    * Added more locator labels
    * Enforce valid locator labels
    * Prevent mounting empty locator spans
    * Properly exclude locator brackets from showing up in suffix

--- source/common/modules/markdown-editor/parser/citation-parser.ts
+++ source/common/modules/markdown-editor/parser/citation-parser.ts
@@ -17,34 +17,62 @@
  * END HEADER
  */
 
+import { type SyntaxNode } from '@lezer/common'
 import { type InlineParser, type Element as MDElement } from '@lezer/markdown'
 
+// See https://github.com/bwiernik/schema/blob/ff67ae11347a4fb444ef839d96549540e9516cc1/schemas/input/csl-citation.json#L144 ff
+export type CSL_LOCATOR_TERM = 'article-locator' | 'book' | 'canon' | 'chapter'
+    | 'column' | 'elocation' | 'equation' | 'figure' | 'folio' | 'issue' | 'line'
+    | 'note' | 'opus' | 'page' | 'paragraph' | 'part' | 'rule' | 'section'
+    | 'sub-verbo' | 'supplement' | 'table' | 'timestamp' | 'title-locator'
+    | 'verse' | 'volume'
+
 /**
- * The locatorLabels have been sourced from the Citr library. Basically it's
- * just a map with valid CSL locator labels and an array of possible natural
- * labels which a user might want to write (instead of the standardized labels).
- * We provide them in lowercase here to match citation parts against them case-
- * insensitively.
+ * The locatorLabels have been sourced from the CSL locale files. These are the
+ * label strings that will trigger the parser to detect an explicit locator
+ * label. The programmatic labels are the keys of this Record, and the labels
+ * that users can use in various languages are the strings in the corresponding
+ * arrays. As of now, only the French, German, and English locator labels have
+ * been added to these lists of labels, since these are the three largest
+ * communities of Zettlr. Going forward, adding more languages is straight-
+ * forward; it just requires some time. But note that citeproc will
+ * automatically use the correct language; i.e., using the label "pp." to denote
+ * pages will correctly render "S." if using the German language for the output.
  *
  * @var {{ [key: string]: string[] }}}
  */
-const locatorLabels: Record<string, string[]> = {
-  book: [ 'Buch', 'Bücher', 'B.', 'book', 'books', 'bk.', 'bks.', 'livre', 'livres', 'liv.' ].map(e => e.toLowerCase()),
-  chapter: [ 'Kapitel', 'Kap.', 'chapter', 'chapters', 'chap.', 'chaps', 'chapitre', 'chapitres' ].map(e => e.toLowerCase()),
-  column: [ 'Spalte', 'Spalten', 'Sp.', 'column', 'columns', 'col.', 'cols', 'colonne', 'colonnes' ].map(e => e.toLowerCase()),
-  figure: [ 'Abbildung', 'Abbildungen', 'Abb.', 'figure', 'figures', 'fig.', 'figs' ].map(e => e.toLowerCase()),
-  folio: [ 'Blatt', 'Blätter', 'Fol.', 'folio', 'folios', 'fol.', 'fols', 'fᵒ', 'fᵒˢ' ].map(e => e.toLowerCase()),
-  issue: [ 'Nummer', 'Nummern', 'Nr.', 'number', 'numbers', 'no.', 'nos.', 'numéro', 'numéros', 'nᵒ', 'nᵒˢ' ].map(e => e.toLowerCase()),
-  line: [ 'Zeile', 'Zeilen', 'Z', 'line', 'lines', 'l.', 'll.', 'ligne', 'lignes' ].map(e => e.toLowerCase()),
-  note: [ 'Note', 'Noten', 'N.', 'note', 'notes', 'n.', 'nn.' ].map(e => e.toLowerCase()),
-  opus: [ 'Opus', 'Opera', 'op.', 'opus', 'opera', 'opp.' ].map(e => e.toLowerCase()),
-  page: [ 'Seite', 'Seiten', 'S.', 'page', 'pages', 'p.', 'pp.' ].map(e => e.toLowerCase()),
-  paragraph: [ 'Absatz', 'Absätze', 'Abs.', '¶', '¶¶', 'paragraph', 'paragraphs', 'para.', 'paras', 'paragraphe', 'paragraphes', 'paragr.' ].map(e => e.toLowerCase()),
-  part: [ 'Teil', 'Teile', 'part', 'parts', 'pt.', 'pts', 'partie', 'parties', 'part.' ].map(e => e.toLowerCase()),
-  section: [ 'Abschnitt', 'Abschnitte', 'Abschn.', '§', '§§', 'section', 'sections', 'sec.', 'secs', 'sect.' ].map(e => e.toLowerCase()),
-  'sub verbo': [ 'sub verbo', 'sub verbis', 's.&#160;v.', 's.&#160;vv.', 's.v.', 's.vv.' ].map(e => e.toLowerCase()),
-  verse: [ 'Vers', 'Verse', 'V.', 'verse', 'verses', 'v.', 'vv.', 'verset', 'versets' ].map(e => e.toLowerCase()),
-  volume: [ 'Band', 'Bände', 'Bd.', 'Bde.', 'volume', 'volumes', 'vol.', 'vols.' ].map(e => e.toLowerCase())
+const locatorLabels: Record<CSL_LOCATOR_TERM, string[]> = {
+  'article-locator': [ 'Art.', 'Artikel',  'art.', 'arts.', 'article', 'articles' ],
+  book: [ 'Buch', 'Bücher', 'B.', 'book', 'books', 'bk.', 'bks.', 'livre', 'livres', 'liv.' ],
+  canon: [ 'can.', 'cann.', 'canon', 'canons' ],
+  chapter: [ 'Kapitel', 'Kap.', 'chapter', 'chapters', 'c.', 'cc.', 'chap.', 'chaps.', 'chapitre', 'chapitres' ],
+  column: [ 'Spalte', 'Spalten', 'Sp.', 'column', 'columns', 'col.', 'cols', 'colonne', 'colonnes' ],
+  elocation: [ 'emplact', 'emplacement', 'emplacements', 'loc.', 'locs.', 'location', 'locations' ],
+  equation: [ 'équation', 'équations', 'eq.', 'eqq.', 'equation', 'equations' ],
+  figure: [ 'Abbildung', 'Abbildungen', 'Abb.', 'figure', 'figures', 'fig.', 'figs' ],
+  folio: [ 'Blatt', 'Blätter', 'Fol.', 'folio', 'folios', 'fol.', 'fols', 'fᵒ', 'fᵒˢ' ],
+  issue: [ 'Nummer', 'Nummern', 'Nr.', 'number', 'numbers', 'no.', 'nos.', 'numéro', 'numéros', 'nᵒ', 'nᵒˢ' ],
+  line: [ 'Zeile', 'Zeilen', 'Z', 'line', 'lines', 'l.', 'll.', 'ligne', 'lignes' ],
+  note: [ 'Note', 'Noten', 'N.', 'note', 'notes', 'n.', 'nn.' ],
+  opus: [ 'Opus', 'Opera', 'op.', 'opus', 'opera', 'opp.' ],
+  page: [ 'Seite', 'Seiten', 'S.', 'page', 'pages', 'p.', 'pp.' ],
+  paragraph: [ 'Absatz', 'Absätze', 'Abs.', '¶', '¶¶', 'paragraph', 'paragraphs', 'para.', 'paras', 'paragraphe', 'paragraphes', 'paragr.' ],
+  part: [ 'Teil', 'Teile', 'part', 'parts', 'pt.', 'pts', 'partie', 'parties', 'part.' ],
+  rule: [ 'règle', 'règles', 'r.', 'rr.', 'rule', 'rules' ],
+  section: [ 'Abschnitt', 'Abschnitte', 'Abschn.', '§', '§§', 'section', 'sections', 'sec.', 'secs', 'sect.' ],
+  'sub-verbo': [ 'sub verbo', 'sub verbis', 's.&#160;v.', 's.&#160;vv.', 's.v.', 's.vv.' ],
+  supplement: [ 'supp.', 'supps.', 'supplement', 'supplements' ],
+  table: [ 'tableau', 'tableaux', 'tab.', 'tbl.', 'tbls.', 'table', 'tables' ],
+  timestamp: [],
+  'title-locator': [ 'titre', 'titres', 'tit.', 'titt.', 'title', 'titles' ],
+  verse: [ 'Vers', 'Verse', 'V.', 'verse', 'verses', 'v.', 'vv.', 'verset', 'versets' ],
+  volume: [ 'Band', 'Bände', 'Bd.', 'Bde.', 'volume', 'volumes', 'vol.', 'vols.' ]
+}
+
+// Normalization: Convert everything to lowercase so that small typos don't trip
+// the parser up.
+for (const key in locatorLabels) {
+  locatorLabels[key as CSL_LOCATOR_TERM] = locatorLabels[key as CSL_LOCATOR_TERM].map(e => e.toLowerCase())
 }
 
 // Compress the locator labels into a 1d string list for easier access.
@@ -77,10 +105,181 @@ const CHAR = {
   CURLY_CLOSE: 125
 }
 
-// Contains character codes for upper/lower case roman numerals (i, v, x, c, d,
-// m and I, V, X, C, D, M).
-const ROMAN_NUMERAL_CODES = [ 73, 105, 86, 118, 88, 120, 67, 99, 68, 100, 77, 109 ]
+// Character code points for upper/lower case roman numerals (CDILMVX).
+const ROMAN_NUMERAL_CODES = [
+  67, 68, 73, 76, 77, 86, 88, // Uppercase
+  99, 100, 105, 108, 109, 118, 120 // Lowercase
+]
 
+/**
+ * Record of all valid citation node names.
+ */
+const NODES = {
+  CITATION: 'Citation',
+  MARK: 'CitationMark',
+  PREFIX: 'CitationPrefix',
+  AUTHORFLAG: 'CitationSuppressAuthorFlag',
+  AT: 'CitationAtSign',
+  KEY: 'CitationCitekey',
+  LOCATOR: 'CitationLocator',
+  SUFFIX: 'CitationSuffix'
+}
+
+/**
+ * Describes a single citation item. Composite citations have only one such item
+ * but regular in-text citations can have multiple ones, divided by semicolons.
+ */
+export interface CiteItem {
+  /**
+   * Citekey -- required.
+   */
+  id: string
+  /**
+   * Locator (only a numerical range)
+   */
+  locator?: string
+  /**
+   * The locator label (what the locator describes). Defaults to "page."
+   */
+  label?: keyof typeof locatorLabels
+  /**
+   * Whether the citekey has the suppress author flag.
+   */
+  'suppress-author'?: boolean
+  /**
+   * Do not use.
+   * @internal
+   */
+  'author-only'?: boolean
+  /**
+   * Anything before the citekey.
+   */
+  prefix?: string
+  /**
+   * Anything after the citekey or locator.
+   */
+  suffix?: string
+}
+
+/**
+ * A full citation cluster.
+ */
+export interface Citation {
+  /**
+   * Start in the source
+   */
+  from: number
+  /**
+   * End in the source
+   */
+  to: number
+  /**
+   * Raw source string
+   */
+  source: string
+  /**
+   * Whether its composite (@AuthorYear [p. 23]) or not ([@AuthorYear, p. 23]).
+   */
+  composite: boolean
+  /**
+   * All items in this citation. Length === 1 if composite is true.
+   */
+  items: CiteItem[]
+}
+
+/**
+ * Utility function that takes a Citation node and the Markdown source and turns
+ * it into valid CiteItems that can be passed to the citeproc library.
+ *
+ * @param   {SyntaxNode}  node      The Citation node. Function throws an error
+ *                                  if the node is malformed.
+ * @param   {string}      markdown  The Markdown source.
+ *
+ * @return  {CiteItem[]}            The citation items.
+ */
+export function nodeToCiteItem (node: SyntaxNode, markdown: string): Citation {
+  if (node.type.name !== 'Citation') {
+    throw new Error(`Expected a Citation node, received type ${node.type.name}`)
+  }
+
+  const items: CiteItem[] = []
+
+  // Now, enter that node and iterate over its children. Citation nodes are flat
+  // so that we can collect them one after another.
+  let child = node.firstChild
+
+  // Composite essentially just means an inline citation where the author
+  // name(s) is/are part of the sentence [@AuthorYear has said -> Author (Year) has said]
+  const composite = child !== null && child.type.name !== NODES.MARK // Mark here implies square bracket open
+
+  let prefix = undefined
+  let citekey = undefined
+  let locator = undefined
+  let label = undefined
+  let suffix = undefined
+  let suppressAuthor = undefined
+
+  while (child !== null) {
+    if (child.type.name === NODES.PREFIX) {
+      prefix = markdown.slice(child.from, child.to)
+    } else if (child.type.name === NODES.KEY) {
+      citekey = markdown.slice(child.from, child.to)
+    } else if (child.type.name === NODES.LOCATOR) {
+      locator = markdown.slice(child.from, child.to)
+      // Check for an explicit label
+      const lcloc = locator.toLowerCase()
+      const explicitLabel = allValidLocatorLabels.find(x => lcloc.startsWith(x + ' '))
+      if (explicitLabel !== undefined) {
+        for (const [ key, values ] of Object.entries(locatorLabels)) {
+          if (values.some(x => lcloc.startsWith(x + ' '))) {
+            label = key as CSL_LOCATOR_TERM
+            // Remove the label from the locator
+            locator = locator.slice(explicitLabel.length + 1)
+            break
+          }
+        }
+      }
+    } else if (child.type.name === NODES.SUFFIX) {
+      suffix = markdown.slice(child.from, child.to)
+    } else if (child.type.name === NODES.AUTHORFLAG) {
+      suppressAuthor = true
+    } else if (child.type.name === NODES.MARK && markdown.slice(child.from, child.to) === ';') {
+      // A mark can often be ignored, but if it's a semicolon, we have to flush
+      // the state into the cite items and reset.
+      if (citekey !== undefined) {
+        items.push({
+          id: citekey,
+          locator, prefix, suffix, label,
+          'suppress-author': suppressAuthor
+        })
+      }
+      prefix = undefined
+      citekey = undefined
+      locator = undefined
+      label = undefined
+      suffix = undefined
+      suppressAuthor = undefined
+    }
+
+    child = child.nextSibling
+  }
+
+  if (citekey !== undefined) {
+    items.push({
+      id: citekey,
+      locator, prefix, suffix, label,
+      'suppress-author': suppressAuthor
+    })
+  }
+
+  return {
+    from: node.from, to: node.to,
+    source: markdown.slice(node.from, node.to),
+    composite, items
+  }
+}
+
+// Here follows the actual parser
 export const citationParser: InlineParser = {
   name: 'citations',
   // This inline parser must be run before the Link parser, as
@@ -169,7 +368,7 @@ export const citationParser: InlineParser = {
         // since both was less readable than I have wished for. So instead I
         // use individual if-branches that usually end in a `continue` or break.
         // Note that some of those branches do not, meaning they are essentially
-        // "fall-through."
+        // 'fall-through.'
 
         // Iteration setup
         const prevCh = ctx.char(i - 1) // Might be Number.NaN
@@ -190,7 +389,7 @@ export const citationParser: InlineParser = {
             parts.push(ctx.elt('CitationLocator', locatorStart, i))
           } else if (locatorEnd > -1 && locatorEnd < i) {
             // Locator has been finalized -> suffix.
-            parts.push(ctx.elt('CitationSuffix', locatorEnd, i))
+            parts.push(ctx.elt('CitationSuffix', locatorInBrackets ? locatorEnd + 1 : locatorEnd, i))
           } else if (citekeyEnd < 0) {
             // Non-bracketed citekey with no locator and no suffix.
             parts.push(ctx.elt('CitationCitekey', citekeyStart, i))
@@ -241,7 +440,7 @@ export const citationParser: InlineParser = {
           }
 
           parts.push(ctx.elt('CitationAtSign', i, i + 1))
-          citekeyStart = i + 1 // Key excludes the "@"
+          citekeyStart = i + 1 // Key excludes the '@'
           continue
         }
         
@@ -318,7 +517,11 @@ export const citationParser: InlineParser = {
           if (locatorInBrackets && ch === CHAR.CURLY_CLOSE) {
             // Curly brackets locators are easy
             locatorEnd = i
-            parts.push(ctx.elt('CitationLocator', locatorStart, locatorEnd))
+            // Bracketed locators can be empty ({}) -> in that case do not add
+            // it to the syntax tree.
+            if (locatorEnd > locatorStart) {
+              parts.push(ctx.elt('CitationLocator', locatorStart, locatorEnd))
+            }
             parts.push(ctx.elt('CitationMark', i, i + 1))
             continue
           } else if (((ch < 48 || ch > 57) && !ROMAN_NUMERAL_CODES.includes(ch) && ch !== CHAR.HYPHEN)) {
