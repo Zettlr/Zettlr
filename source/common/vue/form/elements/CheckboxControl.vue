dd0e057c3acf7dcbedf63ef6d48606dc8ab9a441
Hendrik Erz <hendrik@zettlr.com>
2025-09-25 11:22:49 +0200

    fix: Styling issue with overlong info labels on checkboxes
    NOTE: This now re-enables toggling options when clicking outside the label, but that's how it needs to be.

--- source/common/vue/form/elements/CheckboxControl.vue
+++ source/common/vue/form/elements/CheckboxControl.vue
@@ -92,7 +92,7 @@ body {
     // Some space for the checkbox itself, and then only as much as necessary to
     // fit in the label (this prevents checking/unchecking the checkbox if the
     // user clicks far beyond the label string)
-    grid-template-columns: @input-size * 2 max-content;
+    grid-template-columns: @input-size * 2 auto;
     grid-template-rows: 100%;
     grid-template-areas: "input label";
     align-items: center;
