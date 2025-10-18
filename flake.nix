4f4699c0bb345bcff25306f4f2dbbf689846baa0
MrProgrammerMan <jonas.baugerud@gmail.com>
2025-10-18 12:07:49 +0200

    feat(flake): imrpove flake structure with flake-parts

--- flake.nix
+++ flake.nix
@@ -2,26 +2,32 @@
   description = "Zettlr markdown editor";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = inputs @ { nixpkgs, ... }: 
-  let  
-    system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      buildInputs = with pkgs; [
-        nodejs_20
-        yarn
-        python3
-        pkg-config
-        hunspell
-        hunspellDicts.en-us
-        electron
+  outputs = inputs @ { flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
       ];
-      shellHook = ''
-        export ELECTRON_OVERRIDE_DIST_PATH=$(dirname $(which electron))
-        echo "Electron override set to $ELECTRON_OVERRIDE_DIST_PATH"
-      '';
+      perSystem = { config, pkgs, ... }: {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nodejs_20
+            yarn
+            python3
+            pkg-config
+            hunspell
+            hunspellDicts.en-us
+            electron
+          ];
+          shellHook = ''
+            export ELECTRON_OVERRIDE_DIST_PATH=$(dirname $(which electron))
+            echo "Electron override set to $ELECTRON_OVERRIDE_DIST_PATH"
+          '';
+        };
+      };
     };
-  };
 }
