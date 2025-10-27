7b01eaea0cdc230deb75d984aad776042356be87
benniekiss <63211101+benniekiss@users.noreply.github.com>
2025-10-27 09:53:21 -0400

    refactor to handle optional urls
    * this parser comes before the native one, so we have to handle all link
    * permutations

--- source/common/modules/markdown-editor/parser/link-parser.ts
+++ source/common/modules/markdown-editor/parser/link-parser.ts
@@ -25,7 +25,7 @@ const linkTitleRe = /(?:^|[ \t]+)(?:"((?:\\.|[^"])+)"|'((?:\\.|[^'])+)'|\(((?:\\
 
 export const linkParser: InlineParser = {
   name: 'link-parser',
-  before: 'Autolink',
+  before: 'Link',
   parse: (ctx, next, pos) => {
     if (next === 91) { // 91 === '['
       return ctx.addDelimiter(LinkDelimiter, pos, pos + 1, true, false)
@@ -35,10 +35,10 @@ export const linkParser: InlineParser = {
       return ctx.addDelimiter(LinkDelimiter, pos, pos + 2, true, false)
     }
 
-    const match = linkClosingRe.exec(ctx.text.slice(pos - ctx.offset))
-    if (!match?.groups) { return -1 }
-
-    const opening = ctx.findOpeningDelimiter(LinkDelimiter)
+    let opening = null
+    if (next === 93) { // 93 === ']'
+      opening = ctx.findOpeningDelimiter(LinkDelimiter)
+    }
     if (opening === null) { return -1 }
 
     const delim = ctx.getDelimiterAt(opening)
@@ -53,50 +53,58 @@ export const linkParser: InlineParser = {
 
     ctx.addDelimiter(LinkDelimiter, pos, pos + 1, false, true)
 
-    let url = match.groups.url
+    // Since this parser comes before the native link parser,
+    // we have to handle links without URLs.
+    const match = linkClosingRe.exec(ctx.text.slice(pos - ctx.offset))
+    const urlContents = []
+    if (match?.groups) {
+      let url = match.groups.url
 
-    // The url may contain additional parenthesis, so we need
-    // to count the internal ones to track potential matching pairs
-    // to find the external matching closing one.
-    let depth = 0
-    let stop = 0
-    while (stop <= url.length) {
-      const char = url.charAt(stop)
+      // The url may contain additional parenthesis, so we need
+      // to count the internal ones to track potential matching pairs
+      // to find the external matching closing one.
+      let depth = 0
+      let stop = 0
+      while (stop <= url.length) {
+        const char = url.charAt(stop)
 
-      // Found the closing parenthesis
-      if (char === ')' && depth === 0) { break }
+        // Found the closing parenthesis
+        if (char === ')' && depth === 0) { break }
 
-      if (char === ')') { depth-- }
-      if (char === '(') { depth++ }
+        if (char === ')') { depth-- }
+        if (char === '(') { depth++ }
 
-      stop++
+        stop++
+      }
+
+      url = url.substring(0, stop)
+
+      let destination = url
+
+      const title = linkTitleRe.exec(destination)
+      const urlParts = []
+
+      if (title) {
+        destination = url.substring(0, title.index)
+
+        const linkTitleText = title[1] || title[2] || title[3]
+        urlParts.push(ctx.elt('LinkTitle', pos + 2 + title.index, pos + 2 + linkTitleText.length))
+      }
+
+      urlParts.push(ctx.elt('URL', pos + 2, pos + 2 + destination.length))
+
+      const openingUrlMark = ctx.elt('LinkMark', pos + 1, pos + 2)
+      const closingUrlMark = ctx.elt('LinkMark', pos + 2 + url.length, pos + 3 + url.length)
+
+      urlContents.push(openingUrlMark, ...urlParts, closingUrlMark)
     }
-
-    url = url.substring(0, stop)
-
-    let destination = url
-
-    const title = linkTitleRe.exec(destination)
-    const linkParts = []
-
-    if (title) {
-      destination = url.substring(0, title.index)
-
-      const linkTitleText = title[1] || title[2] || title[3]
-      linkParts.push(ctx.elt('LinkTitle', pos + 2 + title.index, pos + 2 + linkTitleText.length))
-    }
-
-    linkParts.push(ctx.elt('URL', pos + 2, pos + 2 + destination.length))
 
     const openingMark = ctx.elt('LinkMark', delim.from, delim.to)
     const closingMark = ctx.elt('LinkMark', pos, pos + 1)
 
-    const openingUrlMark = ctx.elt('LinkMark', pos + 1, pos + 2)
-    const closingUrlMark = ctx.elt('LinkMark', pos + 2 + url.length, pos + 3 + url.length)
-
     // This child node structure mirrors the codemirror Link structure
-    const children = [ openingMark, ...linkContents, closingMark, openingUrlMark, ...linkParts, closingUrlMark ]
+    const children = [ openingMark, ...linkContents, closingMark, ...urlContents ]
 
-    return ctx.addElement(ctx.elt(isLink ? 'Link' : 'Image', delim.from, pos + 3 + url.length, children))
+    return ctx.addElement(ctx.elt(isLink ? 'Link' : 'Image', delim.from, pos + (match ? match[0].length : 1), children))
   }
 }
