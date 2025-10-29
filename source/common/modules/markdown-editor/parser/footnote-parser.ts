da2a0c98400da7458df2081b1469d5182edf7204
benniekiss <63211101+benniekiss@users.noreply.github.com>
2025-10-29 13:28:34 -0400

    dont defer taking content

--- source/common/modules/markdown-editor/parser/footnote-parser.ts
+++ source/common/modules/markdown-editor/parser/footnote-parser.ts
@@ -54,8 +54,10 @@ export const footnoteParser: InlineParser = {
       return -1
     }
 
+    const children = ctx.takeContent(opening)
+
     ctx.addDelimiter(FootnoteDelimiter, pos, pos + 1, false, true)
-    return ctx.addElement(ctx.elt('Footnote', delim.from, pos + 1, isInline ? ctx.takeContent(opening) : undefined))
+    return ctx.addElement(ctx.elt('Footnote', delim.from, pos + 1, isInline ? children : undefined))
   }
 }
 
