d358bbe751e04d6a5f92673f7609a3d989c74275
benniekiss <63211101+benniekiss@users.noreply.github.com>
2025-11-21 07:15:14 -0500

    [Fix] Update rendering mode immediately on statusbar change (#6010)

--- source/common/modules/markdown-editor/renderers/index.ts
+++ source/common/modules/markdown-editor/renderers/index.ts
@@ -24,7 +24,7 @@ import { renderMermaid } from './render-mermaid'
 import { renderTables } from '../table-editor'
 import { renderIframes } from './render-iframes'
 import { renderEmphasis } from './render-emphasis'
-import { configField, type EditorConfiguration } from '../util/configuration'
+import { configField, configUpdateEffect, type EditorConfiguration } from '../util/configuration'
 import type { EditorView } from '@codemirror/view'
 import { hasMarkdownExt } from 'source/common/util/file-extention-checks'
 import { trans } from 'source/common/i18n-renderer'
@@ -32,45 +32,70 @@ import type { StatusbarItem } from '../statusbar'
 
 const renderCompartment = new Compartment()
 
-const transactionExtender = EditorState.transactionExtender.from(configField, config => transaction => {
-  const ext: Extension[] = [renderMermaid]
-  if (config.renderingMode === 'preview' && config.renderImages) {
-    ext.push(renderImages)
-  }
-  if (config.renderingMode === 'preview' && config.renderLinks) {
-    ext.push(renderLinks)
-  }
-  if (config.renderingMode === 'preview' && config.renderMath) {
-    ext.push(renderMath)
-  }
-  if (config.renderingMode === 'preview' && config.renderTasks) {
-    ext.push(renderTasks)
-  }
-  if (config.renderingMode === 'preview' && config.renderHeadings) {
-    ext.push(renderHeadings)
-  }
-  if (config.renderingMode === 'preview' && config.renderCitations) {
-    ext.push(renderCitations)
-  }
-  if (config.renderingMode === 'preview' && config.renderTables) {
-    ext.push(renderTables)
-  }
-  if (config.renderingMode === 'preview' && config.renderIframes) {
-    ext.push(renderIframes)
-  }
-  if (config.renderingMode === 'preview' && config.renderEmphasis) {
-    ext.push(renderEmphasis)
+/* Adds or removes an extension from a list of extensions based on the value of enabled */
+function updateExtension (renderer: Extension, enabled: boolean|undefined, ext: Extension[]) {
+  const idx = ext.indexOf(renderer)
+
+  // Renderer is enabled and not in the list
+  if (enabled === true && idx === -1) { ext.push(renderer) }
+  // Renderer is disabled and in the list
+  if (enabled === false && idx > -1) { ext.splice(idx, 1) }
+
+  return ext
+}
+
+/* Configures the enabled renderer extensions, optionally updating an existing set of extensions */
+function configureRenderers (config: Partial<EditorConfiguration>, ext?: Extension[]) {
+  if (ext === undefined) {
+    ext = []
   }
 
-  const currentState = renderCompartment.get(transaction.state) as Extension[]|undefined
-  if (currentState === undefined) {
-    return { effects: renderCompartment.reconfigure(ext) }
-  } else {
-    // Compare the two states. Reconfigure if they differ
-    for (const extension of ext.concat(currentState)) {
-      if (currentState.includes(extension) !== ext.includes(extension)) {
-        return { effects: renderCompartment.reconfigure(ext) }
+  if (!ext.includes(renderMermaid)) {
+    ext.push(renderMermaid)
+  }
+
+  if (config.renderingMode === 'raw') {
+    ext = [renderMermaid]
+  }
+
+  if (config.renderingMode === 'preview') {
+    updateExtension(renderImages, config.renderImages, ext)
+    updateExtension(renderLinks, config.renderLinks, ext)
+    updateExtension(renderMath, config.renderMath, ext)
+    updateExtension(renderTasks, config.renderTasks, ext)
+    updateExtension(renderHeadings, config.renderHeadings, ext)
+    updateExtension(renderCitations, config.renderCitations, ext)
+    updateExtension(renderTables, config.renderTables, ext)
+    updateExtension(renderIframes, config.renderIframes, ext)
+    updateExtension(renderEmphasis, config.renderEmphasis, ext)
+  }
+
+  return ext
+}
+
+/**
+ * A TransactionExtender that reconfigures the renderer extension compartment in response
+ * to a configUpdateEffect
+ */
+const modeSwitcher = EditorState.transactionExtender.from(configField, config => transaction => {
+  for (const effect of transaction.effects) {
+    if (effect.is(configUpdateEffect)) {
+      const overrides = {
+        renderingMode: effect.value.renderingMode ?? config.renderingMode,
+        renderImages: effect.value.renderImages ?? config.renderImages,
+        renderLinks: effect.value.renderLinks ?? config.renderLinks,
+        renderMath: effect.value.renderMath ?? config.renderMath,
+        renderTasks: effect.value.renderTasks ?? config.renderTasks,
+        renderHeadings: effect.value.renderHeadings ?? config.renderHeadings,
+        renderCitations: effect.value.renderCitations ?? config.renderCitations,
+        renderTables: effect.value.renderTables ?? config.renderTables,
+        renderIframes: effect.value.renderIframes ?? config.renderIframes,
+        renderEmphasis: effect.value.renderEmphasis ?? config.renderEmphasis,
       }
+
+      const ext = renderCompartment.get(transaction.state) as Extension[]|undefined
+      return { effects: renderCompartment.reconfigure(configureRenderers(overrides, ext)) }
+
     }
   }
 
@@ -80,40 +105,12 @@ const transactionExtender = EditorState.transactionExtender.from(configField, co
 /**
  * Configures the renderers that are active in the given Markdown state.
  *
- * @param   {Partial<EditorConfiguration>}  config  An optional initial config
+ * @param   {EditorConfiguration}  config  An optional initial config
  *
  * @return  {Extension}                             The extension set
  */
-export function renderers (config?: Partial<EditorConfiguration>): Extension {
-  const ext: Extension[] = [renderMermaid]
-  if (config?.renderingMode === 'preview' && config?.renderImages === true) {
-    ext.push(renderImages)
-  }
-  if (config?.renderingMode === 'preview' && config?.renderLinks === true) {
-    ext.push(renderLinks)
-  }
-  if (config?.renderingMode === 'preview' && config?.renderMath === true) {
-    ext.push(renderMath)
-  }
-  if (config?.renderingMode === 'preview' && config?.renderTasks === true) {
-    ext.push(renderTasks)
-  }
-  if (config?.renderingMode === 'preview' && config?.renderHeadings === true) {
-    ext.push(renderHeadings)
-  }
-  if (config?.renderingMode === 'preview' && config?.renderCitations === true) {
-    ext.push(renderCitations)
-  }
-  if (config?.renderingMode === 'preview' && config?.renderTables === true) {
-    ext.push(renderTables)
-  }
-  if (config?.renderingMode === 'preview' && config?.renderIframes === true) {
-    ext.push(renderIframes)
-  }
-  if (config?.renderingMode === 'preview' && config?.renderEmphasis === true) {
-    ext.push(renderEmphasis)
-  }
-  return [ transactionExtender, renderCompartment.of(ext) ]
+export function renderers (config: EditorConfiguration): Extension {
+  return [ modeSwitcher, renderCompartment.of(configureRenderers(config)) ]
 }
 
 /**
