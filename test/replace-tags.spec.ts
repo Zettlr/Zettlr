427d96c49625c792e907e7f2d28a8db3757d0f53
benniekiss <63211101+benniekiss@users.noreply.github.com>
2025-12-07 09:37:23 -0500

    [Fix] Sanitize existing tags to lowercase in `replaceTags` (#6009)

--- test/replace-tags.spec.ts
+++ test/replace-tags.spec.ts
@@ -16,12 +16,14 @@
 import replaceTags from '@common/util/replace-tags'
 import { strictEqual } from 'assert'
 
-const testDocument = `---
+const testDocument = `\
+---
 title: "A simple test document"
 author: Zettlr
 keywords:
 - one tag
 - second-tag
+- Mixed-case-Tag
 ---
 
 # A simple test document
@@ -31,7 +33,7 @@ of the replaceTags function to properly replace tags within files.
 
 It contains a YAML frontmatter that defines one keyword with a space in it that
 cannot occur anywhere else in the document; as well as the #second-tag that now
-also occurs within here.
+also occurs within here. And a #MiXeD-case-TaG.
 
 For example, if we have a [link](#second-tag), it shouldn't be replaced.
 Similarly with [link](example.com#second-tag).`
@@ -39,12 +41,16 @@ Similarly with [link](example.com#second-tag).`
 const replaceTagsTesters = [
   {
     // With spaces -> with spaces
-    oldTag: 'one tag', newTag: 'A new keyword', expected: `---
+    oldTag: 'one tag',
+    newTag: 'A new keyword',
+    expected: `\
+---
 title: "A simple test document"
 author: Zettlr
 keywords:
 - A new keyword
 - second-tag
+- Mixed-case-Tag
 ---
 
 # A simple test document
@@ -54,19 +60,23 @@ of the replaceTags function to properly replace tags within files.
 
 It contains a YAML frontmatter that defines one keyword with a space in it that
 cannot occur anywhere else in the document; as well as the #second-tag that now
-also occurs within here.
+also occurs within here. And a #MiXeD-case-TaG.
 
 For example, if we have a [link](#second-tag), it shouldn't be replaced.
 Similarly with [link](example.com#second-tag).`
   },
   {
     // No spaces -> no spaces
-    oldTag: 'second-tag', newTag: 'new-keyword', expected: `---
+    oldTag: 'second-tag',
+    newTag: 'new-keyword',
+    expected: `\
+---
 title: "A simple test document"
 author: Zettlr
 keywords:
 - one tag
 - new-keyword
+- Mixed-case-Tag
 ---
 
 # A simple test document
@@ -76,19 +86,23 @@ of the replaceTags function to properly replace tags within files.
 
 It contains a YAML frontmatter that defines one keyword with a space in it that
 cannot occur anywhere else in the document; as well as the #new-keyword that now
-also occurs within here.
+also occurs within here. And a #MiXeD-case-TaG.
 
 For example, if we have a [link](#second-tag), it shouldn't be replaced.
 Similarly with [link](example.com#second-tag).`
   },
   {
     // No spaces -> spaces
-    oldTag: 'second-tag', newTag: 'New keyword', expected: `---
+    oldTag: 'second-tag',
+    newTag: 'New keyword',
+    expected: `\
+---
 title: "A simple test document"
 author: Zettlr
 keywords:
 - one tag
 - New keyword
+- Mixed-case-Tag
 ---
 
 # A simple test document
@@ -98,19 +112,23 @@ of the replaceTags function to properly replace tags within files.
 
 It contains a YAML frontmatter that defines one keyword with a space in it that
 cannot occur anywhere else in the document; as well as the  that now
-also occurs within here.
+also occurs within here. And a #MiXeD-case-TaG.
 
 For example, if we have a [link](#second-tag), it shouldn't be replaced.
 Similarly with [link](example.com#second-tag).`
   },
   {
     // Spaces -> No spaces
-    oldTag: 'one tag', newTag: 'one-tag', expected: `---
+    oldTag: 'one tag',
+    newTag: 'one-tag',
+    expected: `\
+---
 title: "A simple test document"
 author: Zettlr
 keywords:
 - one-tag
 - second-tag
+- Mixed-case-Tag
 ---
 
 # A simple test document
@@ -120,7 +138,33 @@ of the replaceTags function to properly replace tags within files.
 
 It contains a YAML frontmatter that defines one keyword with a space in it that
 cannot occur anywhere else in the document; as well as the #second-tag that now
-also occurs within here.
+also occurs within here. And a #MiXeD-case-TaG.
+
+For example, if we have a [link](#second-tag), it shouldn't be replaced.
+Similarly with [link](example.com#second-tag).`
+  },
+  {
+    // Case insensitive
+    oldTag: 'Mixed-case-Tag',
+    newTag: 'all-lowercase-tag',
+    expected: `\
+---
+title: "A simple test document"
+author: Zettlr
+keywords:
+- one tag
+- second-tag
+- all-lowercase-tag
+---
+
+# A simple test document
+
+This #document contains some tags as well as some edge-cases to test the ability
+of the replaceTags function to properly replace tags within files.
+
+It contains a YAML frontmatter that defines one keyword with a space in it that
+cannot occur anywhere else in the document; as well as the #second-tag that now
+also occurs within here. And a #all-lowercase-tag.
 
 For example, if we have a [link](#second-tag), it shouldn't be replaced.
 Similarly with [link](example.com#second-tag).`
