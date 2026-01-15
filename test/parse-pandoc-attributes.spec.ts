6c046d7b0e1a2027b5aab372b3459eff6a5b9cae
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-01-15 11:05:24 -0500

    update tests
    * removed some braces from tests
    * simplified test structure since the function no longer errors

--- test/parse-pandoc-attributes.spec.ts
+++ test/parse-pandoc-attributes.spec.ts
@@ -16,9 +16,9 @@ import { deepStrictEqual } from 'assert'
 import { ParsedPandocAttributes, parsePandocAttributes } from 'source/common/pandoc-util/parse-pandoc-attributes'
 
 const tests: Array<{ input: string, output: ParsedPandocAttributes|'logs-error' }> = [
-  { input: 'width=50%', output: 'logs-error' }, // Missing braces. See NOTE below.
+  { input: 'width=50%', output: { properties: { width: '50%' } } }, // Attributes without braces
   { input: '{width=50%}', output: { properties: { width: '50%' } } }, // Simple parsing
-  { input: '{ width = 50% }', output: {} }, // Spaces between = not supported
+  { input: ' width = 50% ', output: {} }, // Spaces between = not supported
   { input: '{ width= 50% }', output: {} }, // Spaces between = not supported
   { input: '{ width =50% }', output: {} }, // Spaces between = not supported
   { input: '{width=10}', output: { properties: { width: '10px' } } }, // Parsing width without unit
@@ -27,7 +27,7 @@ const tests: Array<{ input: string, output: ParsedPandocAttributes|'logs-error' 
   { input: '{key=some long value}', output: { properties: { key: 'some' } } }, // Parsing malformed unquoted values
   {
     // Longer test with all available classes
-    input: '{#some-id .class1 .class2 width=50% height=25 disabled=false style="font-size: 12px;"}',
+    input: '#some-id .class1 .class2 width=50% height=25 disabled=false style="font-size: 12px;"',
     output: {
       id: 'some-id',
       classes: ['class1', 'class2'],
@@ -51,27 +51,8 @@ const tests: Array<{ input: string, output: ParsedPandocAttributes|'logs-error' 
 
 describe('Utility#parsePandocAttributes()', function () {
   for (const test of tests) {
-    // NOTE: Changed on January 10, 2026 by Hendrik Erz. Reason: Parsing link
-    // attributes should not throw errors, since this function is also called in
-    // parsing contexts, and throwing errors would completely abort the parsing,
-    // rendering entirely white documents that cannot be edited. Instead, the
-    // function has been changed to simply return an empty record and log the
-    // error to the console.
-    if (test.output === 'logs-error') {
-      it(`should log an error for string "${test.input}"`, function () {
-        const oldError = console.error
-        let hasLoggedError = false
-        console.error = (...args) => {
-          if (args[0] instanceof Error) hasLoggedError = true
-        }
-        parsePandocAttributes(test.input)
-        deepStrictEqual(hasLoggedError, true)
-        console.error = oldError
-      })
-    } else {
-      it(`should parse the attribute string "${test.input}"`, function () {
-        deepStrictEqual(parsePandocAttributes(test.input), test.output)
-      })
-    }
+    it(`should parse the attribute string "${test.input}"`, function () {
+      deepStrictEqual(parsePandocAttributes(test.input), test.output)
+    })
   }
 })
