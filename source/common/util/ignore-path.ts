5d298bd042ea7671546add33226a1d123ed43e29
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-02-10 13:47:05 -0500

    Update source/common/util/ignore-path.ts
    Co-authored-by: Hendrik Erz <hendrik@zettlr.com>

--- source/common/util/ignore-path.ts
+++ source/common/util/ignore-path.ts
@@ -56,7 +56,7 @@ export const WATCHDOG_IGNORE_RE: RegExp[] = WATCHDOG_IGNORE_PATHS
   .map(re => new RegExp(`${prefixRE}${re}${suffixRE}`, 'i'))
 
 export const IGNORE_PATH_RE: RegExp[] = IGNORE_PATHS
-  .map(re => new RegExp(`${re_prefix}${re}${re_suffix}`, 'i'))
+  .map(re => new RegExp(`${prefixRE}${re}${suffixRE}`, 'i'))
 
 /**
  * Check if the given filename is a dot file or folder.
