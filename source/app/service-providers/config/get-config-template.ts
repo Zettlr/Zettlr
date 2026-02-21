919c8225ea5076f0afca83aadfa7ed65276ccf90
benniekiss <63211101+benniekiss@users.noreply.github.com>
2026-02-21 13:50:12 -0500

    add force-lint button to toolbar

--- source/app/service-providers/config/get-config-template.ts
+++ source/app/service-providers/config/get-config-template.ts
@@ -248,6 +248,7 @@ export interface ConfigOptions {
     showMarkdownLinkButton: boolean
     showMarkdownImageButton: boolean
     showMarkdownMakeTaskListButton: boolean
+    showMarkdownLintButton: boolean
     showInsertTableButton: boolean
     showInsertFootnoteButton: boolean
     showDocumentInfoText: boolean
@@ -515,6 +516,7 @@ export function getConfigTemplate (): ConfigOptions {
       showMarkdownLinkButton: true,
       showMarkdownImageButton: true,
       showMarkdownMakeTaskListButton: true,
+      showMarkdownLintButton: true,
       showInsertTableButton: true,
       showInsertFootnoteButton: true,
       showDocumentInfoText: true,
